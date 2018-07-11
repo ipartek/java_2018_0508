fbd79b1b9610ccfd42b186ca266e861270e54a7e
Curso <Curso@Formacion-TOS14.ipartekaula.com>
2018-07-11 12:13:33 +0200

    Ejercicio numero 3 terminado

---
+++ src/com/ipartek/formacion/ejercicios/basicos/iniciales/Ejercicio3.java
@@ -0,0 +1,36 @@
+package com.ipartek.formacion.ejercicios.basicos.iniciales;
+/**
+ * Escribe un programa Java que declare una variable entera N y asígnale un valor. A continuación escribe las instrucciones que realicen los siguientes:<br>
+Incrementar N en 77.<br>
+Decrementarla en 3.<br>
+Duplicar su valor.<br>
+Si por ejemplo N = 1 la salida del programa será:<br>
+Valor inicial de N = 1<br>
+N + 77 = 78 <br>
+N - 3 = 75 <br>
+N * 2 = 150 <br>
+ * @author Curso
+ *
+ */
+
+public class Ejercicio3 {
+	public static void main(String[] args) {
+		
+		int n = 8;
+        System.out.println("Valor del incremento de n es  = " +  (n +77) );
+        System.out.println("Valor de decrementar de n es  = " +  (n -3) );
+        System.out.println("Valor de duplicar de n es  = " +  (n *2) );
+       
+       
+
+		
+		
+		
+			
+		}
+}
+		
+	
+	
+
+
