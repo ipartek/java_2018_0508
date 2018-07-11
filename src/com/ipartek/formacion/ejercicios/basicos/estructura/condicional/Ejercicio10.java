e55efc1f42a5b7e0046a7212143d8d190ea89aff
alexanderDevWeb <[dev.alexander.web@gmail.com]>
2018-07-11 12:50:26 +0200

    Ejercicio 10 condicionales

--- src/com/ipartek/formacion/ejercicios/basicos/estructura/condicional/Ejercicio10.java
+++ src/com/ipartek/formacion/ejercicios/basicos/estructura/condicional/Ejercicio10.java
@@ -1,5 +1,7 @@
 package com.ipartek.formacion.ejercicios.basicos.estructura.condicional;
 
+import java.util.Scanner;
+
 /**
  * Programa que lea una variable entera mes y compruebe si el valor corresponde
  * a un mes de 30 días, de 31 o de 28.
@@ -9,7 +11,22 @@ package com.ipartek.formacion.ejercicios.basicos.estructura.condicional;
 public class Ejercicio10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner teclado = new Scanner(System.in);
+		int m =0;
+		do {
+			System.out.print("Introduce el numero correspondiente a un mes: ");
+			m = teclado.nextInt();
+		}while(m<=0 || m>12);
+		
+		if( (m<=7 && m%2==0 && m!=2) || (m>8 && m%2!=0)) {
+			System.out.println("Tiene 30 dias");
+		}else if(m==2){
+			System.out.println("Tiene 28 dias");
+		}else {
+			System.out.println("Tiene 31 dias");
+		}
+		teclado.close();
 
 	}
 
