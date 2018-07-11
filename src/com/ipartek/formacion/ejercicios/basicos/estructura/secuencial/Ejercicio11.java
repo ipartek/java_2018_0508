a00018331bb112b851f0e11f862588fff9a3c6c1
Curso <Curso@Formacion-TOSH9.ipartekaula.com>
2018-07-11 12:52:17 +0200

    11. Programa que lea un número entero N de 5 cifras y muestre sus cifras desde el principio como en el ejemplo.

--- src/com/ipartek/formacion/ejercicios/basicos/estructura/secuencial/Ejercicio11.java
+++ src/com/ipartek/formacion/ejercicios/basicos/estructura/secuencial/Ejercicio11.java
@@ -21,22 +21,17 @@ public class Ejercicio11 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		numero = Integer.parseInt(br.readLine());
 
-		/*
-		 * primero = numero/10000; segundo = numero/1000; tercero = numero/100; cuarto =
-		 * numero/10; quinto = numero;
-		 * 
-		 * 
-		 * System.out.println("Primera cifra de " + numero + " -> " + primero);
-		 * System.out.println("Segunda central de " + numero + " -> " + segundo);
-		 * System.out.println("Tercera cifra  de " + numero + " -> " + tercero);
-		 * System.out.println("Cuarto cifra  de " + numero + " -> " + cuarto);
-		 * System.out.println("Quinto cifra  de " + numero + " -> " + quinto);
-		 */
+		primero = numero / 10000;
+		segundo = numero / 1000 % 10;
+		tercero = numero / 100 % 10;
+		cuarto = numero / 10 % 10;
+		quinto = numero % 10;
 
-		System.out.println(numero / 10000);
-		System.out.println(numero / 1000 % 10);
-		System.out.println(numero / 100);
-		System.out.println(numero / 10);
-		System.out.println(numero);
+		System.out.println("Primera cifra de " + numero + " -> " + primero);
+		System.out.println("Segunda central de " + numero + " -> " + segundo);
+		System.out.println("Tercera cifra  de " + numero + " -> " + tercero);
+		System.out.println("Cuarto cifra  de " + numero + " -> " + cuarto);
+		System.out.println("Quinto cifra  de " + numero + " -> " + quinto);
+
 	}
 }
