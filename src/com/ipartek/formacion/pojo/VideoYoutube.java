554695ceb24482ce05d928a4fb0d2c42ea230e37
Andrea Perez <anmapemi06@hotmail.com>
2018-07-11 21:14:23 +0200

    Falta pulir el codigo

---
+++ src/com/ipartek/formacion/pojo/VideoYoutube.java
@@ -0,0 +1,76 @@
+package com.ipartek.formacion.pojo;
+
+public class VideoYoutube {
+
+	private long id;
+	private String codigo;
+	private String titulo;
+	private String autor;
+	private String idBorrar;
+
+	public VideoYoutube() {
+		super();
+	}
+
+	public VideoYoutube(long id, String codigo, String titulo, String autor) {
+		this();
+		this.id = id;
+		this.codigo = codigo;
+		this.titulo = titulo;
+		this.autor = autor;
+	}
+
+	public VideoYoutube(String idBorrar, String codigo, String titulo, String autor) {
+		super();
+		this.idBorrar = idBorrar;
+		this.codigo = codigo;
+		this.titulo = titulo;
+		this.autor = autor;
+	}
+
+	public String getIdBorrar() {
+		return idBorrar;
+	}
+
+	public void setIdBorrar(String idBorrar) {
+		this.idBorrar = idBorrar;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	@Override
+	public String toString() {
+		return "id=" + id + "\n codigo=" + codigo + "\n titulo=" + titulo + "\n autor=" + autor;
+	}
+
+}
---
+++ src/com/ipartek/formacion/videos/GestorVideos.java
@@ -0,0 +1,152 @@
+package com.ipartek.formacion.videos;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+import javax.sound.midi.Soundbank;
+
+import com.ipartek.formacion.pojo.VideoYoutube;
+
+/**
+ * 
+ * @author andreaperez
+ *
+ */
+
+public class GestorVideos {
+
+	static VideoYoutube[] videos = new VideoYoutube[2];
+	static Scanner sc = new Scanner(System.in);
+	static String continuar = "s";
+	static int respuesta;
+	static String autor;
+	static String titulo;
+	static String codigoVideo;
+
+	public static void main(String[] args) {
+
+		videos[0] = new VideoYoutube(1, "Vj1190w58UM", "Uno X Uno", "Manuel Carrasco");
+		videos[1] = new VideoYoutube(2, "Vj1190w58UM", "Uno X Uno", "Manuel Carrasco");
+
+		do {
+			pintarMenu();
+			System.out.print("Inserta el numero de la opcion deseada:");
+			respuesta = sc.nextInt();
+			switch (respuesta) {
+			case 1:
+				listaVideo(videos);
+				break;
+			case 2:
+				agregarVideo();
+				break;
+			case 3:
+
+				System.out.println("    Listado de videos :    ");
+				listaVideo(videos);
+
+				System.out.print("Ingrese el numero del video a eliminar:");
+				respuesta = sc.nextInt();
+
+				if (respuesta == videos.length - 1) {
+					videos[respuesta] = new VideoYoutube("", "", "", "");
+				} else {
+					for (int i = respuesta; i < videos.length - 1; i++) {
+						videos[respuesta] = videos[respuesta + 1];
+					}
+					videos[videos.length - 1] = new VideoYoutube("", "", "", "");
+				}
+
+				listaVideo(videos);
+//				eliminaElement(videos,respuesta);
+//				
+//				System.out.println("    Listado de videos con elemento eliminado:    ");
+//				listaVideo(videos);
+
+				break;
+			case 0:
+				System.out.println(" ADIOS!! ");
+				break;
+
+			}
+
+		} while (respuesta != 0);
+
+	}
+
+	private static void pintarMenu() {
+
+		// TODO pintar menu
+		System.out.println("------------------------------------");
+		System.out.println("--          youtube               --");
+		System.out.println("------------------------------------");
+		System.out.println("    0. salir");
+		System.out.println("------------------------------------");
+		System.out.println("    1. Listar");
+		System.out.println("------------------------------------");
+		System.out.println("    2. Agregar");
+		System.out.println("------------------------------------");
+		System.out.println("    3. Eliminar");
+		System.out.println("------------------------------------");
+
+	}
+
+	private static boolean validarEnt(String cadena) {
+		try {
+			Integer.parseInt(cadena);
+			return true;
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+
+	}
+
+	private static void listaVideo(VideoYoutube[] videos) {
+		System.out.println("\n");
+		System.out.println(" Listando videos:    ");
+		for (int i = 0; i < videos.length; i++) {
+			System.out.println(videos[i].getId() + " " + videos[i].getAutor());
+
+		}
+		System.out.println("\n");
+
+	}
+
+	private static void agregarVideo() {
+		System.out.println("\n");
+		System.out.println(" - INSERTANDO VIDEOS - ");
+		int cont = 0;
+		int i;
+		do {
+
+			System.out.print("inserte autor: ");
+			autor = sc.next();
+
+			System.out.print("inserte titulo: ");
+			titulo = sc.next();
+
+			System.out.print("inserte URL: ");
+			codigoVideo = sc.next();
+
+			System.out.println("guardado registro....");
+
+			for (i = cont; i <= cont; i++) {
+				videos[i].setTitulo(titulo);
+				videos[i].setAutor(autor);
+				videos[i].setCodigo(codigoVideo);
+				videos[i].setId(i + 1);
+			}
+			cont++;
+			i++;
+
+			System.out.println("¿Deseas agregar mas videos? \"s\" si \"n\"no");
+			continuar = sc.next();
+
+		} while (!"n".equalsIgnoreCase(continuar));
+		System.out.println("\n");
+	}
+
+	private static void eliminaElement(VideoYoutube[] arrayVideos, int num) {
+
+	}
+
+}
