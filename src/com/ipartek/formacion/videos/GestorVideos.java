2cce804db93927ccc234ec0268684160886c336d
anderuraga <ander.uraga@gmail.com>
2018-07-12 08:39:26 +0200

    commit para para pull

--- src/com/ipartek/formacion/videos/GestorVideos.java
+++ src/com/ipartek/formacion/videos/GestorVideos.java
@@ -7,10 +7,19 @@ public class GestorVideos {
 	static VideoYoutube[] videos = new VideoYoutube[5];
 	
 	public static void main(String[] args) {
+		
+		cargarVideos();
+		
 		pintarMenu();
 	}
 
 	
+	private static void cargarVideos() {
+		//TODO crear y guadar 5 videos en el array
+		
+	}
+
+
 	private static void pintarMenu() {
 	
 		//TODO pintar menu
