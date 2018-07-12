4eb9f22477c997daa2dfe433c445afdde096dc3e
Curso <Curso@Formacion-TOS15.ipartekaula.com>
2018-07-12 14:28:03 +0200

    mas curro

---
+++ src/com/ipartek/formacion/ejercicios/basicos/estructura/condicional/Ejercicio1.java
@@ -0,0 +1,24 @@
+package com.ipartek.formacion.ejercicios.basicos.estructura.condicional;
+
+/**
+ * Programa Java que lea un número entero por teclado y calcule si es par o
+ * impar.
+ * 
+ * Podemos saber si un número es par si el resto de dividir el número entre 2 es
+ * igual a cero. En caso contrario el número es impar
+ * 
+ * El operador Java que calcula el resto de la división entre dos números
+ * enteros o no es el operador %
+ * 
+ * @author Curso
+ *
+ */
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
--- src/com/ipartek/formacion/model/VideoYoutubeArrayDAO.java
+++ src/com/ipartek/formacion/model/VideoYoutubeArrayDAO.java
@@ -1,27 +1,63 @@
 package com.ipartek.formacion.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.ipartek.formacion.pojo.VideoYoutube;
 
+/**
+ * Clase DAO para gestionar los VideoYoutube con ArrayList Usamos patron
+ * Singleton
+ * 
+ * @see https://es.wikipedia.org/wiki/Singleton#Java
+ * @author Curso
+ *
+ */
+
 public class VideoYoutubeArrayDAO implements CrudAble {
+
+	private static VideoYoutubeArrayDAO INSTANCE = null;
+	private static List<VideoYoutube> lista = null;
+
+	private VideoYoutubeArrayDAO() {
+		lista = new ArrayList<VideoYoutube>();
+	}
+
+	public static synchronized VideoYoutubeArrayDAO getInstance() {
+
+		if (INSTANCE == null) {
+			INSTANCE = new VideoYoutubeArrayDAO();
+		}
+
+		return INSTANCE;
+	}
 
 	@Override
 	public boolean insert(VideoYoutube video) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean resul = false;
+		if (video != null) {
+			resul = lista.add(video);
+		}
+		return resul;
 	}
 
 	@Override
 	public List<VideoYoutube> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return lista;
 	}
 
 	@Override
 	public VideoYoutube getById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+
+		VideoYoutube video = null;
+
+		for (int i = 0; i < lista.size(); i++) {
+			if (lista.get(i).getId()==id)
+				video=lista.get(i);
+			
+		}
+
+		return video;
 	}
 
 	@Override
@@ -36,6 +72,4 @@ public class VideoYoutubeArrayDAO implements CrudAble {
 		return false;
 	}
 
-	
-	
 }
