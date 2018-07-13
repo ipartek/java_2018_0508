6ff37e4037e8a37c2cae08bf25d675814dca1994
Curso <Curso@Formacion-TOSH3.ipartekaula.com>
2018-07-13 13:23:42 +0200

    clase con ejemplo de conjuntos =>hashMap

---
+++ src/com/ipartek/formacion/ejercicios/Conjuntos.java
@@ -0,0 +1,49 @@
+package com.ipartek.formacion.ejercicios;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/**
+ * Se demuestra que los conjuntos no puede contener elementos repetidos.
+ * 
+ * @author andreaPerez
+ *
+ */
+public class Conjuntos {
+
+	public static void main(String[] args) {
+
+		Set<Integer> setOfIntegers = new HashSet<Integer>();
+		setOfIntegers.add(Integer.valueOf(10));
+		setOfIntegers.add(Integer.valueOf(11));
+		setOfIntegers.add(Integer.valueOf(10));
+
+		for (Integer i : setOfIntegers) {
+			System.out.println("Integer value is: " + i);
+		}
+
+		// Ejemplo de hashMap
+		HashMap<String, String> hmPaises = new HashMap<String, String>();
+
+		hmPaises.put("En_en", "Ingles de Inglaterra");
+		hmPaises.put("Es_es", "Español de España");
+		hmPaises.put("Es_eu", "Euskera de España");
+		hmPaises.put("Es_ct", "Catalan de España");
+
+//		for (int i = 0; i < hmPaises.size(); i++) {
+//			System.out.println(hmPaises.values());
+//		}
+
+		for (Entry<String, String> pais : hmPaises.entrySet()) {
+			System.out.println(pais.getKey() + " => " + pais.getValue());
+		}
+		
+		
+		
+		
+	}
+
+}
