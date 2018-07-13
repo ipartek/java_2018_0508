fb5a6b4a54672ba468dfec78f3502b90721d6354
eneko <enekosanchez95@gmail.com>
2018-07-13 14:00:07 +0200

    fixed bugs

--- src/com/ipartek/formacion/model/VideoYoutubeArrayDAOTest.java
+++ src/com/ipartek/formacion/model/VideoYoutubeArrayDAOTest.java
@@ -1,12 +1,15 @@
 package com.ipartek.formacion.model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class VideoYoutubeArrayDAOTest {
