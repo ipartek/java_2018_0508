0eeab5b0a198e93d787e9bc721978bd332f6b66a
valen <valen@LAPTOP-HQBN3GIH>
2018-07-16 20:14:53 +0200

    dfdf

--- src/com/ipartek/formacion/ejercicios/generales/package-info.java
+++ src/com/ipartek/formacion/ejercicios/generales/package-info.java
@@ -1,5 +1,8 @@
 /**
- * 
+ * En esta entrada vamos a explicar dos métodos para realizar el intercambio de valores entre dos variables.
+El primer método de intercambio utiliza una variable auxiliar y el segundo método realiza el intercambio de valores sin utilizar variable auxiliar.
+Intercambio de valores entre dos variables utilizando una variable auxiliar.
+
  */
 /**
  * @author valen
