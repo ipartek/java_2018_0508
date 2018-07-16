f4e1c04fe7e4e869f05ebefd51cfd1e3984bb6c3
jose ignacio <jose ignacio@joseignacio-HP>
2018-07-16 19:35:12 +0200

    numeros romanos corregido

--- src/com/ipartek/formacion/ejercicios/generales/Ejercicio11.java
+++ src/com/ipartek/formacion/ejercicios/generales/Ejercicio11.java
@@ -15,34 +15,46 @@ public class Ejercicio11 {
 		System.out.print("Introduce un numero del 1 al 999: ");
 		Scanner teclado = new Scanner(System.in);
 		int num = teclado.nextInt();
-	
-		List<RomanNumber> list = new ArrayList<RomanNumber>();			
-		list.add(new RomanNumber('M',1000));
-		list.add(new RomanNumber('D',500));
-		list.add(new RomanNumber('C',100));
-		list.add(new RomanNumber('L',50));
-		list.add(new RomanNumber('X',10));
-		list.add(new RomanNumber('V',5));
-		list.add(new RomanNumber('I',1));
-		
-		String str="";
+
+		List<RomanNumber> list = new ArrayList<RomanNumber>();
+		list.add(new RomanNumber('M', 1000));// dos
+		list.add(new RomanNumber('D', 500));// ant
+		list.add(new RomanNumber('C', 100));// dos
+		list.add(new RomanNumber('L', 50));// ant
+		list.add(new RomanNumber('X', 10));// dos
+		list.add(new RomanNumber('V', 5)); // ant
+		list.add(new RomanNumber('I', 1));
+
+		String str = "";
+
+		int j = 0;
+
 		for (RomanNumber romanNumber : list) {
-			if(num<=0) {
+
+			if (num <= 0) {
 				break;
 			}
-			if(num/romanNumber.getCode() >0) {
-				for (int i = 0; i <num/romanNumber.getCode(); i++) {
-					str+=romanNumber.getWord();
+
+			if (num / romanNumber.getCode() > 0) {
+				for (int i = 0; i < num / romanNumber.getCode(); i++) {
+					str += romanNumber.getWord();
 				}
-				num%=romanNumber.getCode();
+				num %= romanNumber.getCode();
 			}
+
+			if (j+1 < list.size()) {
+				if (num / (romanNumber.getCode() - list.get(j + ((j % 2 == 0) ? 2 : 1)).getCode()) > 0) {
+					str += list.get(j + ((j % 2 == 0) ? 2 : 1)).getWord()+""+ romanNumber.getWord();
+					num %= romanNumber.getCode() - list.get(j + ((j % 2 == 0) ? 2 : 1)).getCode();
+
+				}
+			}
+
+			j++;
 		}
-		System.out.println("El numero romano es: "+str);
+		System.out.println("El numero romano es: " + str);
 		teclado.close();
 
-
 	}
-	
-	
 
 }
