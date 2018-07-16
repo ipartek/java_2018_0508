f8640c3c90bdfa8a58173687a12ad5ddbac48c2b
Adriana Prado <adriana.prado@opendeusto.es>
2018-07-17 01:21:20 +0200

    Cuatro primeros ejercicios de estructura secuencial hechos.

---
+++ src/com/ipartek/formacion/ejercicios/basicos/estructura/secuencial/Ejercicio1.java
@@ -0,0 +1,30 @@
+package com.ipartek.formacion.ejercicios.basicos.estructura.secuencial;
+
+import java.util.Scanner;
+
+/***
+ * Programa Java que lea dos números enteros por teclado y los muestre por pantalla.
+ * @author user
+ *
+ */
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1;
+		int num2;
+		
+		Scanner sc =  new Scanner(System.in);
+		
+		System.out.println("Introduzca un primer numero: ");
+		num1 = sc.nextInt();
+		System.out.println("El primero numero tecleado es el " + num1);
+		
+		System.out.println("Introduzca un segundo numero: ");
+		num2 = sc.nextInt();
+		System.out.println("El segundo numero tecleado es el " + num2);
+		
+		sc.close();
+	}
+
+}
---
+++ src/com/ipartek/formacion/ejercicios/basicos/estructura/secuencial/Ejercicio2.java
@@ -0,0 +1,24 @@
+package com.ipartek.formacion.ejercicios.basicos.estructura.secuencial;
+
+import java.util.Scanner;
+
+/***
+ * Programa Java que lea un nombre y muestre por pantalla: 
+ * “Buenos dias nombre_introducido”.
+ * @author user
+ *
+ */
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca su nombre: ");
+		nombre = sc.nextLine();
+		
+		System.out.println("Buenos dias " + nombre);
+		sc.close();
+	}
+
+}
