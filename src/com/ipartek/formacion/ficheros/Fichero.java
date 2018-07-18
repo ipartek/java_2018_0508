bcdb00e36c382c5b819d1318dca78e3f4d290841
guillemoSanchez <[guisanza@gmail.com]>
2018-07-18 10:06:10 +0200

    Leer/Escribir Ficheros + buscartesoro

---
+++ src/com/ipartek/formacion/ficheros/Fichero.java
@@ -0,0 +1,132 @@
+package com.ipartek.formacion.ficheros;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Fichero {
+
+	public static void main(String[] args) {
+
+		// crearFichero();
+
+		// leerFichero("Ficheros/hola.txt");
+
+		buscarTesoro("C:/Desarrollo/eclipse-workspace/java_2018_0508/Ficheros/");
+
+		listarUnidadesPC();
+	}
+
+	private static void listarUnidadesPC() {
+		File[] roots = File.listRoots();
+
+		for (int i = 0; i < roots.length; i++) {
+			System.out.println(roots[i]);
+		}
+
+	}
+
+	private static void buscarTesoro(String pathname) {
+
+		File folder = new File(pathname);
+		File[] listOfFiles = folder.listFiles();
+		String txt = "";
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+
+			if (listOfFiles[i].isDirectory()) {
+				buscarTesoro(pathname + "/" + listOfFiles[i].getName());
+			}
+
+			if (listOfFiles[i].isFile()) {
+				txt = leerFichero(pathname + "/" + listOfFiles[i].getName());
+				if (txt.contains("tesoro")) {
+					System.out.println("Encontrado!");
+					System.out.println("Esta en el archivo: " + pathname + "/" + listOfFiles[i].getName());
+					break;
+				}
+
+			}
+
+		}
+
+	}
+
+	private static void crearFichero() {
+
+		File f = new File("Ficheros/temp.txt");
+		System.out.println("Path" + f.getAbsolutePath());
+
+		String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula malesuada aliquam. Nullam lacus sem, bibendum et condimentum ac, maximus eget sem. Fusce dictum lectus sit amet ligula ullamcorper, sit amet pellentesque lorem molestie. Donec magna diam, sodales nec dictum at, rutrum at est. Ut rhoncus in lacus sed lacinia. Nullam nibh lorem, molestie nec aliquet et, vestibulum id mauris. Mauris augue est, varius at vestibulum non, rhoncus sed leo. Ut consectetur dictum libero, et laoreet quam tincidunt ac. Fusce congue pellentesque dictum. Praesent aliquet turpis sit amet lorem pellentesque gravida. Nam feugiat commodo ultrices. Mauris dignissim viverra metus vitae imperdiet. Duis feugiat volutpat quam, ut ullamcorper augue tristique id. Maecenas lorem lorem, fermentum vel nibh a, blandit placerat neque. Sed sed facilisis enim. Nulla scelerisque purus vitae nibh venenatis, a accumsan magna dictum.";
+
+		if (f.exists()) {
+			System.out.println("Existe el fichero");
+
+		} else {
+			try {
+				f.createNewFile();
+
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+		}
+
+		FileWriter fw = null;
+		PrintWriter pw = null;
+		try {
+			fw = new FileWriter("c:/prueba.txt");
+			pw = new PrintWriter(fw);
+			pw.print(lorem);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+				fw.close();
+				pw.close();
+			} catch (Exception e) {
+				System.out.println("ERROR al cerrar el fichero");
+			}
+
+		}
+
+	}
+
+	private static String leerFichero(String fileName) {
+
+		File f = new File(fileName);
+		String txt = "";
+		// System.out.println("Vamos a comenzar a leer fichero...");
+		try {
+			// usamos los stream para mejorar la lectura o la escritura
+			FileReader fr = new FileReader(f);
+			BufferedReader bf = new BufferedReader(fr); // buffer
+			try {
+
+				String linea;
+				while ((linea = bf.readLine()) != null) {
+					// System.out.println(linea);
+					txt += linea + "\n";
+				}
+
+			} finally {
+				bf.close();
+				fr.close();
+
+			}
+		} catch (IOException e) {
+			System.out.println("Caught exception while processing file: " + e.getMessage());
+		}
+		// System.out.println("Terminada lectura del fichero");
+
+		return txt;
+
+	}
+
+}
