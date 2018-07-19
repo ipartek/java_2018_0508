6d18860a054708a7ce47d589690cc8b3871345cc
Curso <Curso@Formacion-TOS14.ipartekaula.com>
2018-07-19 09:37:00 +0200

    Terminado el apartado generales

--- src/com/ipartek/formacion/ejercicios/generales/Ejercicio12.java
+++ src/com/ipartek/formacion/ejercicios/generales/Ejercicio12.java
@@ -68,30 +68,58 @@ public class Ejercicio12 {
 			try {
 				opcion = (char) System.in.read();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
+			
 				e.printStackTrace();
 			}
 
 		} while (Character.toUpperCase(opcion) != 'C' && Character.toUpperCase(opcion) != 'D');
 		if (Character.toUpperCase(opcion) == 'C') {
-			System.out.println("Texto cifrado = " + cifradoCesar(texto, codigo));
+			System.out.println("Texto cifrado = " + cifradoCesar1(texto, codigo));
 		} else {
 			System.out.println("Texto decifrado = " + descifradoCesar(texto, codigo));
 		}
 
 	}
 
-	private static String descifradoCesar(String texto, int codigo) {
+	public static String cifradoCesar1(String texto, int codigo) {
 		StringBuilder cifrado = new StringBuilder();
 		codigo = codigo % 26;
-		for (int i = 0; i < texto.length();i++)
-		return null;
-		return texto;
+		for (int i = 0; i < texto.length(); i++) {
+			if (texto.charAt(i) >= 'a' && texto.charAt(i) <= 'z') {
+				if ((texto.charAt(i) + codigo) > 'z') {
+					cifrado.append((char) (texto.charAt(i) + codigo - 26));
+				} else {
+					cifrado.append((char) (texto.charAt(i) + codigo));
+				}
+			} else if (texto.charAt(i) >= 'A' && texto.charAt(i) <= 'Z') {
+				if ((texto.charAt(i) + codigo) > 'Z') {
+					cifrado.append((char) (texto.charAt(i) + codigo - 26));
+				} else {
+					cifrado.append((char) (texto.charAt(i) + codigo));
+				}
+			}
+		}
+		return cifrado.toString();
 	}
 
-	private static String cifradoCesar(String texto, int codigo) {
-		// TODO Auto-generated method stub
-		return null;
+	public static String descifradoCesar(String texto, int codigo) {
+		StringBuilder cifrado = new StringBuilder();
+		codigo = codigo % 26;
+		for (int i = 0; i < texto.length(); i++) {
+			if (texto.charAt(i) >= 'a' && texto.charAt(i) <= 'z') {
+				if ((texto.charAt(i) - codigo) < 'a') {
+					cifrado.append((char) (texto.charAt(i) - codigo + 26));
+				} else {
+					cifrado.append((char) (texto.charAt(i) - codigo));
+				}
+			} else if (texto.charAt(i) >= 'A' && texto.charAt(i) <= 'Z') {
+				if ((texto.charAt(i) - codigo) < 'A') {
+					cifrado.append((char) (texto.charAt(i) - codigo + 26));
+				} else {
+					cifrado.append((char) (texto.charAt(i) - codigo));
+				}
+			}
+		}
+		return cifrado.toString();
 	}
-
 }
