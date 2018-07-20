cda2af8fec17bc2ea4ad1745776ca1a33936507f
Curso <Curso@Formacion-TOSH2.ipartekaula.com>
2018-07-20 12:34:45 +0200

    Subida examen avanzamos

--- src/com/ipartek/formacion/uf2216/GestorRevistas.java
+++ src/com/ipartek/formacion/uf2216/GestorRevistas.java
@@ -1,5 +1,9 @@
 package com.ipartek.formacion.uf2216;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -29,6 +33,10 @@ public class GestorRevistas {
 	static private final int NUM_MIN_PAGINAS = 1;
 	static private final boolean FORMATO_DIGITAL = true;
 	static private final boolean FORMATO_PAPEL = false;
+	static private final String FORMATO_DIGITAL_TEXTO = "digital";
+	static private final String FORMATO_PAPEL_TEXTO = "papel";
+	static private final char OPCION_SI = 's';
+	static private final char OPCION_NO = 'n';
 
 	public static void main(String[] args) {
 		
@@ -161,9 +169,11 @@ public class GestorRevistas {
 		int numPaginas = -1;
 		String formatoTexto = null;
 		boolean formato = false;
+		char eleccion = 'n';
 		
 		try {
 			sc.nextLine();
+			System.out.println();
 			do {//Pedir por teclado titulo de la revista
 				try {
 					System.out.println("Introduce el titulo de la revista, por favor: (Minimo 3 letras, Maximo 150)");
@@ -176,6 +186,7 @@ public class GestorRevistas {
 
 			do {//Pedir por teclado isbn de la revista
 				try {
+					System.out.println();
 					System.out.println("Introduce el ISBN de la revista, por favor: (10 numeros)");
 					isbn = sc.nextLine();
 				} catch (Exception e) {
@@ -187,6 +198,7 @@ public class GestorRevistas {
 			
 			do {//Pedir por teclado numero de paginas de la revista
 				try {
+					System.out.println();
 					System.out.println("Introduce el numero de paginas de la revista, por favor: (Minimo 1)");
 					numPaginas = sc.nextInt();
 				} catch (Exception e) {
@@ -195,8 +207,10 @@ public class GestorRevistas {
 				}
 			} while (numPaginas < NUM_MIN_PAGINAS);
 			sc.nextLine();
+			
 			do {//Pedir por teclado formato de la revista
 				try {
+					System.out.println();
 					System.out.println("Introduce el formato de la revista, por favor: (Digital o Papel)");
 					formatoTexto = sc.nextLine();
 					formatoTexto = formatoTexto.toLowerCase();
@@ -204,7 +218,7 @@ public class GestorRevistas {
 					System.out.println("Formato incorrecto, repita por favor.");
 					e.printStackTrace();
 				}
-			} while (formatoTexto.isEmpty());
+			} while (!(FORMATO_DIGITAL_TEXTO.equals(formatoTexto) || FORMATO_PAPEL_TEXTO.equals(formatoTexto)));
 			
 			//Convertir la opcion de formato de String a boolean para luego introducirla
 			if(formatoTexto.equals("digital")) {
@@ -213,11 +227,39 @@ public class GestorRevistas {
 				formato = FORMATO_PAPEL;
 			}
 			
-			Revista revistaNueva = new Revista(titulo, isbn, numPaginas, formato);
-			dao.insert(revistaNueva);
-			System.out.println("Revista nueva introducida correctamente.");
+			//Mostrar los datos introducidos
+			System.out.println();
+			System.out.println("Datos de la revista nueva:");
+			System.out.println("---------------------------");
+			System.out.println("Titulo: " + titulo);
+			System.out.println("ISBN: " + isbn);
+			System.out.println("Num. Paginas: " + numPaginas);
+			System.out.println("Formato: " + formatoTexto);
+			
+			do {//Pedir al usuario si desea guardar la nueva revista
+				try {
+					System.out.println();
+					System.out.println("¿Desea introducirla en la lista de revistas? s/n");
+					eleccion = sc.next().charAt(0);
+					eleccion = Character.toLowerCase(eleccion);
+				}catch(Exception e) {
+					System.out.println("Introduce una opcion valida, por favor (s/n).");
+					e.printStackTrace();
+				}
+			}while(!(eleccion != OPCION_SI || eleccion != OPCION_NO));
+			
+			if(eleccion == OPCION_SI) {
+				Revista revistaNueva = new Revista(titulo, isbn, numPaginas, formato);
+				dao.insert(revistaNueva);
+				System.out.println();
+				System.out.println("Revista nueva introducida correctamente.");
+			}else {
+				System.out.println();
+				System.out.println("Se han descartado los datos.");
+			}
 			
 		} catch (Exception e) {
+			System.out.println("Hemos tenido un error y no se ha podido insertar la nueva revista, lo sentimos.");
 			e.printStackTrace();
 		} finally {
 			System.out.println();
@@ -230,8 +272,48 @@ public class GestorRevistas {
 	 * Metodo para crear un archivo TXT con todas las revistas
 	 */
 	private static void crearTxt() {
-		// TODO Auto-generated method stub
 		try {
+			File f = new File("src/com/ipartek/formacion/uf2216/fichero.txt");
+			if (f.exists()) {
+				System.out.println("El fichero existe.");
+			} else {
+				try {
+					f.createNewFile();
+					System.out.println("Fichero creado.");
+				} catch (IOException e) {
+					System.out.println("Error al crear el fichero.");
+					e.printStackTrace();
+				}
+			}
+			
+			//Escribimos las revistas en el fichero
+			FileWriter fichero = null;
+			PrintWriter pw = null;
+			List<Revista> revistas = new ArrayList<Revista>();
+			try {
+				revistas = dao.getAll();
+				fichero = new FileWriter("src/com/ipartek/formacion/uf2216/fichero.txt");
+				pw = new PrintWriter(fichero);
+				for (int i = 0; i < revistas.size(); i++) {
+					pw.println(revistas.get(i));
+				}
+				
+				System.out.println();
+				System.out.println("Las revistas se han guardado en el fichero correctamente.");
+
+			} catch (Exception e) {
+				System.out.println("Error al escribir en el fichero.");
+				e.printStackTrace();
+			} finally {
+				try {
+					if (null != fichero) {
+						fichero.close();
+					}
+				} catch (Exception e2) {
+					System.out.println("Error al intentar cerrar el fichero.");
+					e2.printStackTrace();
+				}
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
