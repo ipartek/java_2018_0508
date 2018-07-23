e868587015dbdeb5ca248c649b2e24272ca43967
Curso <Curso@Formacion-TOS12.ipartekaula.com>
2018-07-23 10:07:36 +0200

    Xml

---
+++ src/com/ipartek/formacion/ejercicios/xml/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package para aprender lo básico con <b>XML</b>
+ * 
+ * @see https://www.tutorialspoint.com/java_xml/java_dom_parser.htm
+ * @author Curso
+ *
+ */
+package com.ipartek.formacion.ejercicios.xml;
