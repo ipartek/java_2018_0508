619a65d022f79db63e4369413d21e452bba2bd84
Curso <Curso@Formacion-TOS10.ipartekaula.com>
2018-07-23 10:06:58 +0200

    ejercicio xml

---
+++ src/com/ipartek/formacion/ejercicios/xml/ParseoXML2Java.java
@@ -0,0 +1,54 @@
+package com.ipartek.formacion.ejercicios.xml;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class ParseoXML2Java {
+
+	public static void main(String[] args) {
+		
+		 System.out.println("Comenzamos parseo");
+		 
+		 try {
+			File inputFile = new File("xml/casa.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(inputFile);
+			doc.getDocumentElement().normalize();
+			
+			NodeList nlCasa = doc.getElementsByTagName("casa");
+			
+			Node nodeCasa = nlCasa.item(0);
+			//System.out.println("Nodo casa texto: " + nodeCasa.getTextContent());
+			
+			NodeList nlChildCasa = nodeCasa.getChildNodes();
+			
+			Node nodeChild;
+			
+			for(int i = 0; i < nlChildCasa.getLength(); i++) {
+				
+				nodeChild = nlChildCasa.item(i);
+				
+				if(nodeChild.getNodeType() == Node.ELEMENT_NODE) {
+					System.out.println("Nombre del elemento: " + nodeChild.getNodeName());
+				
+					System.out.println("Contenido del elemento: " + nodeChild.getTextContent());
+				}else {
+					System.out.println("Nodo tipo TEXT_NODE = " + nodeChild.getNodeType());
+				}
+				
+			}
+			
+		 }catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
---
+++ src/com/ipartek/formacion/ejercicios/xml/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package para aprender lo básico de xml.
+ * @see https://www.tutorialspoint.com/java_xml/java_dom_parser.htm
+ * @author Adrian Garcia
+ *
+ */
+package com.ipartek.formacion.ejercicios.xml;
