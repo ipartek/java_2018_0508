e818b8b4c2bed9a1dc60f6deaae32fb4b6e75433
Windows7 <Windows7@PC>
2018-07-24 21:54:52 +0200

    definitivo

--- jee/src/main/java/com/ipartek/formacion/gestor/libros/controller/JuegoController.java
+++ jee/src/main/java/com/ipartek/formacion/gestor/libros/controller/JuegoController.java
@@ -1,7 +1,6 @@
 package com.ipartek.formacion.gestor.libros.controller;
 
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,75 +13,105 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/juego")
 public class JuegoController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private RequestDispatcher dispatch = null;
 
-	private static final String VIEW_INDEX = "index.jsp";
-	private static final String VIEW_JUEGO = "juego.jsp";
-	private static final String palabra="cesar";
-	char[] respuesta;
+	private static final String palabra = "cesar";
+
+	private static char[] respuesta = { '_', '_', '_', '_', '_' };
 	
-    
+	private static int VIDAS = 0;
+	private static final int MAX_VIDAS = 6;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		System.out.println("Pasamos por juego");
 
-		// 1. recibir parametros
-		String letra = request.getParameter("letra");
-		
-		// 2. validar parametros
+		try {
+			// 1.- Recibir Parametros
 
-		// 3. llamar modelo DAO
+			// 2.- Validar Parametros
 
-		// 4. enviar Atributos vista
-		String palabra = String.format("Palabra %s", letra);
-		
-		request.setAttribute("palabra", palabra);
+			// 3.- LLamar modelo DAO
 
-		// 5. ir a la vista
-		request.getRequestDispatcher("juego.jsp").forward(request, response);
+			// 4.- Enviar atributos a la vista
+
+			request.setAttribute("respuesta", respuesta);
+
+			// 5.- Ir a la vista
+
+			request.getRequestDispatcher("juego.jsp").forward(request, response);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		char c = ' '; 
+		boolean contador = false;
+		
 		try {
-			dispatch= request.getRequestDispatcher(VIEW_INDEX);
-			String letra = request.getParameter("letra");
-					if (letra != null) {
-						if(palabra.toLowerCase().contains(letra)) {
-							request.setAttribute("msg", "Letra correcta");
-							for(int i = 0; i < palabra.length(); i++){
-								if (palabra.charAt(i) == letra.charAt(0)) {
-									request.setAttribute("letra", palabra.charAt(i));
-								}						
-							}
-							request.getRequestDispatcher("juego.jsp").forward(request, response);
-						}else {
-							
-							request.setAttribute("msg", "Letra incorrecta");
-							request.getRequestDispatcher("juego.jsp").forward(request, response);
-						}
-				
-				} else {
-	
-					request.setAttribute("msg", "Por favor introduce una letra");
-					request.getRequestDispatcher("juego.jsp").forward(request, response);
-				}	
+			// 1.- Recibir Parametros
+			c = (char) request.getParameter("letra").toLowerCase().charAt(0);
+
+			// 2.- Validar Parametros
+
+			for (int i = 0; i < palabra.length(); i++) {
+				if (c == palabra.charAt(i)) {
+					respuesta[i] = c;
+					contador=true;
+					break;
+				}
+			}
 			
+			boolean fin = false;
+			
+			if (!contador) {
+				VIDAS++;
+			}else {
+				fin = solucion();
+			}
+			
+			// 3.- LLamar modelo DAO
+
+			// 4.- Enviar atributos a la vista
+
+			
+			if (fin) {
+				request.setAttribute("msg", "Zorionak !!! Has ganado");
+			}else if(VIDAS == MAX_VIDAS){
+				request.setAttribute("msg", "Vaya... no has ganado, otra partidita");
+			}
+			
+			request.setAttribute("resultado", respuesta);
+			request.setAttribute("vidas", VIDAS);
+
+			// 5.- Ir a la vista
+
+			request.getRequestDispatcher("juego.jsp").forward(request, response);
+
 		} catch (Exception e) {
+			// TODO: handle exception
 			e.printStackTrace();
-		} finally {
-			dispatch.forward(request, response);
 		}
+
 	}
 
+	private boolean solucion() {
+		
+		boolean resul = true;
+		
+		for (int i = 0; i < respuesta.length; i++) {
+			if (respuesta[i] == '_') {
+				resul = false;
+			}
+		}
+		
+		return resul;
+		
+	}
 }
