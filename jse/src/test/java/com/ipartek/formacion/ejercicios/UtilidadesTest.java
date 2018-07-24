12b6e95908929c4d60eb7c0e45782d7c07cd4aeb
Curso <Curso@Formacion-TOS11.ipartekaula.com>
2018-07-24 09:18:37 +0200

    Prueba push

--- jse/src/test/java/com/ipartek/formacion/ejercicios/UtilidadesTest.java
+++ jse/src/test/java/com/ipartek/formacion/ejercicios/UtilidadesTest.java
@@ -8,14 +8,14 @@ public class UtilidadesTest {
 
 	@Test
 	public void testBubbleSort() {
-		
-		int[] arrayDesordenado = {0,3,1,8,7,2,5,4,6,9};		
-		int[] arrayOrdenado = Utilidades.bubbleSort(arrayDesordenado);
-		
-		for( int i = 0; i < 9; i++) {
+
+		int[] arrayDesordenado = { 0, 3, 1, 8, 7, 2, 5, 4, 6, 9 };
+		int[] arrayOrdenado = Utilidades.bubbleSort(arrayDesordenado, false);
+
+		for (int i = 0; i < 9; i++) {
 			assertEquals(i, arrayOrdenado[i]);
-		}		
-		
+		}
+
 	}
 
 }
