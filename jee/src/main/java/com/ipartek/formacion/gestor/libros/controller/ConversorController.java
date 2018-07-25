e226dd514da09f02a411056ecfe803eb6732a3e0
Curso <Curso@Formacion-TOS14.ipartekaula.com>
2018-07-25 13:54:35 +0200

    cambios no correctos

--- jee/src/main/java/com/ipartek/formacion/gestor/libros/controller/ConversorController.java
+++ jee/src/main/java/com/ipartek/formacion/gestor/libros/controller/ConversorController.java
@@ -15,23 +15,30 @@ public class ConversorController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private final double PIES_METRO = 3.2808;
 	
+
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		double valor = new Double(request.getParameter("valor"));
-		
-		String tipo = request.getParameter("tipo");
-		double resultado = 0.0;
-		
-		if("m".equals(tipo)) {
-			resultado = valor * PIES_METRO;
-		}else {
-			resultado = valor / PIES_METRO;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		try {
+			double valor = new Double(request.getParameter("valor"));
+
+			String tipo = request.getParameter("tipo");
+			double resultado = 0.0;
+
+			if ("m".equals(tipo)) {
+				resultado = valor * PIES_METRO;
+			} else {
+				resultado = valor / PIES_METRO;
+			}
+			request.setAttribute("resultado", resultado);
+			request.getRequestDispatcher("conversor.jsp").forward(request, response);
+
+		} catch (NumberFormatException e) {
+			
+		} finally {
+			
 		}
-		
-		request.setAttribute("resultado", resultado);
-		
-		request.getRequestDispatcher("conversor.jsp").forward(request, response);
+
 	}
-
 }
-
