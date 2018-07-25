734005035064feecab55c1dd06b9491b0806694e
Curso <Curso@Formacion-TOS10.ipartekaula.com>
2018-07-25 09:24:48 +0200

    Juego ahorcado

--- jee/src/main/java/com/ipartek/formacion/gestor/libros/controller/AhorcadoController.java
+++ jee/src/main/java/com/ipartek/formacion/gestor/libros/controller/AhorcadoController.java
@@ -13,34 +13,33 @@ import javax.servlet.http.HttpServletResponse;
 public class AhorcadoController extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-	private static boolean LETRA_ENCONTRADA = false;
+	private static boolean letraEncontrada = false;
 	private RequestDispatcher dispatch = null;
 	private static final String VIEW_AHORCADO = "ahorcado.jsp";
+	private static final String PALABRA_SECRETA = "cersar";
 	private static final int MAXIMO_FALLOS = 7;
-	private static int NUMERO_FALLOS = 0;
+	private static int numeroFallos = 0;
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		dispatch = request.getRequestDispatcher(VIEW_AHORCADO);
-		dispatch.forward(request, response);
+		
+		doProcess(request, response);
 		
 	}
-	
+
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			
+		doProcess(request, response);
+		
+	}
+	
+	private void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		try {
-			String palabra[] = new String[5];
-			palabra[0] = "c";
-			palabra[1] = "e";
-			palabra[2] = "s";
-			palabra[3] = "a";
-			palabra[4] = "r";
-			
+					
 			dispatch = request.getRequestDispatcher(VIEW_AHORCADO);
-			
-			
 			
 			//1. Recibir parámetros
 			String letraIntroducida = request.getParameter("letra");
@@ -48,28 +47,30 @@ public class AhorcadoController extends HttpServlet {
 			//2. Validar parámetros(si procede)
 			
 			if(letraIntroducida != null && !"".equals(letraIntroducida.trim())) {
+			
+				char letra = Character.toLowerCase(letraIntroducida.charAt(0));
 				
-				for (int i = 0; i < palabra.length; i++) {
+				for (int i = 0; i < PALABRA_SECRETA.length(); i++) {
 					
-					if(letraIntroducida.trim().equals(palabra[i])) {
-						request.setAttribute("letra" + i, letraIntroducida);
-						request.setAttribute("encontrado", "La letra " + letraIntroducida + " es correcta");
-						LETRA_ENCONTRADA = true;
-						dispatch = request.getRequestDispatcher(VIEW_AHORCADO);
-						break;
+					if(letra == PALABRA_SECRETA.charAt(i)) {
+						request.setAttribute("letra" + i, letra);
+						request.setAttribute("encontrado", "La letra " + letra + " es correcta");
+						letraEncontrada = true;
+						//dispatch = request.getRequestDispatcher(VIEW_AHORCADO);
+						//break;
 						
 					}
 				}
 				
-				if(LETRA_ENCONTRADA == false) {
-					NUMERO_FALLOS++;
+				if(letraEncontrada == false) {
+					numeroFallos++;
 					request.setAttribute("encontrado", "La letra " + letraIntroducida + " no es correcta");
-					dispatch = request.getRequestDispatcher(VIEW_AHORCADO);
+					//dispatch = request.getRequestDispatcher(VIEW_AHORCADO);
 				}
 				
-				if(NUMERO_FALLOS == MAXIMO_FALLOS) {
+				if(numeroFallos == MAXIMO_FALLOS) {
 					request.setAttribute("eliminado", "Lo sentimos, no has acertado la palabra");
-					dispatch = request.getRequestDispatcher(VIEW_AHORCADO);
+					//dispatch = request.getRequestDispatcher(VIEW_AHORCADO);
 				}
 				
 			}else {
@@ -84,15 +85,6 @@ public class AhorcadoController extends HttpServlet {
 		}finally {
 			dispatch.forward(request, response);
 		}
-		
-		
-		//3. Llamar modelo DAO
-		
-		
-		//4. Enviar atributos al la vista
-		
-		
-		//5. Ir a la vista
 		
 	}
 	
