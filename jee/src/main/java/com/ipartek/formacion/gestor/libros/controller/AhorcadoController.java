3fe19e4e983e776088bb781a5483db75bec50f4d
Curso <Curso@Formacion-TOS14.ipartekaula.com>
2018-07-25 09:28:45 +0200

    Corregido en clase

--- jee/src/main/java/com/ipartek/formacion/gestor/libros/controller/AhorcadoController.java
+++ jee/src/main/java/com/ipartek/formacion/gestor/libros/controller/AhorcadoController.java
@@ -13,39 +13,39 @@ public class AhorcadoController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	private static final int MAX_INTENTOS = 7;
-	private static final String PALABRA = "valeria";
-	
+	private static final String PALABRA = "cesar";
+
 	private String palabraUsuario = "_______";
 	private int contador = 0;
-	
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		String let = request.getParameter("let");
-		
-		if(let !=null && !let.trim().equals("") && let.length() == 1) {
-			
-			if(contador < MAX_INTENTOS) {
+
+		if (let != null && !let.trim().equals("") && let.length() == 1) {
+
+			if (contador < MAX_INTENTOS) {
 				contador++;
-				for(int i=0;i<PALABRA.length();i++) {
-					if(PALABRA.charAt(i)==let.charAt(0)) {
-						
+				for (int i = 0; i < PALABRA.length(); i++) {
+					if (PALABRA.toLowerCase().charAt(i) == let.toLowerCase().charAt(0)) {
+						palabraUsuario = palabraUsuario.substring(0, i) + let + palabraUsuario.substring(i + 1);
+
 					}
 				}
 				request.setAttribute("respuesta", palabraUsuario);
 				request.getRequestDispatcher("ahorcado.jsp").forward(request, response);
-			}else {
+			} else {
 				request.setAttribute("respuesta", "Game Over");
-				request.getRequestDispatcher("ahorcado.jsp").forward(request, response);				
+				request.getRequestDispatcher("ahorcado.jsp").forward(request, response);
 			}
 
-		}else {
+		} else {
 			request.setAttribute("respuesta", "El texto introducido tiene que ser UNA letra");
 			request.getRequestDispatcher("ahorcado.jsp").forward(request, response);
 		}
-		
+
 	}
-	
+
 }
