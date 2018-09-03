cf564907a2245b75e97424cc2d24fbc1cb7e5d4a
Curso <Curso@Formacion-TOS14.ipartekaula.com>
2018-09-03 10:56:59 +0200

    prueba de commit

---
+++ html5/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * funcion inicial que se ejecuta al cargar
+ * la pagina principal,mediante el evento 'onload' del <body>
+ */
+function init() {
+    console.log('Iniciado');
+    setTimeout(function(){
+
+        let loading = document.getElementById('loading');
+        loading.innerHTML="Ready";
+
+    },2000)
+}
