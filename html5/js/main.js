37cb1e247e7321d9bb52e4885d2c28f12ad6630b
Curso <Curso@Formacion-TOS13.ipartekaula.com>
2018-09-03 14:22:46 +0200

    Posicionamiento

--- html5/js/main.js
+++ html5/js/main.js
@@ -1,12 +1,12 @@
 /**
-* Funcion inicial que se ejecuta al iniciar
-* la pagina principal, mediante el evento 'onload' del body
-*/
+ * Funcion inicial que se ejecuta al iniciar
+ * la pagina principal, mediante el evento 'onload' del body
+ */
 function init() {
     console.log('Iniciado');
 
-    setTimeout(function() {
+    setTimeout(function () {
         let parrafo = document.getElementById('loading');
         parrafo.innerHTML = "Ready";
-    },2000);
+    }, 2000);
 }
