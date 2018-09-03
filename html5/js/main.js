2e237570f504a45a36c04912df89388d6e4b77ed
Curso <Curso@Formacion-TOS13.ipartekaula.com>
2018-09-03 09:30:29 +0200

    HTML5

---
+++ html5/js/main.js
@@ -0,0 +1,12 @@
+/**
+* Funcion inicial que se ejecuta al iniciar
+* la pagina principal, mediante el evento 'onload' del body
+*/
+function init() {
+    console.log('Iniciado');
+
+    setTimeout(function() {
+        let parrafo = document.getElementById('loading');
+        parrafo.innerHTML = "Ready";
+    },2000);
+}
