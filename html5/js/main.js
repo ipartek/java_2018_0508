3dbf88bfc060bcaf94da1ae72634ef011b160969
Curso <Curso@Formacion-TOS11.ipartekaula.com>
2018-09-04 14:25:50 +0200

    Ejercicios 4-9-2018

--- html5/js/main.js
+++ html5/js/main.js
@@ -14,3 +14,16 @@ function init() {
     },2000)
    
 }
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
