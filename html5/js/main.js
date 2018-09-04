3484020dc30a6a8788a6a1c5efd52db854703c10
Curso <Curso@Formacion-TOS12.ipartekaula.com>
2018-09-04 12:00:02 +0200

    html5

--- html5/js/main.js
+++ html5/js/main.js
@@ -3,6 +3,7 @@
  * Función inicial que se ejecuta al cargar la página principal, 
  * mediante el elemento 'onload' del <BODY>
  */
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous"></link>
 function init(){
     console.log('Iniciado');
     
@@ -10,8 +11,6 @@ function init(){
         let loading = document.getElementById('loading');
         loading.innerHTML = "Ready";  
     },2000);
-    
-    
-
-
+      
 }
+
