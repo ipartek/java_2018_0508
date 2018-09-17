adfa804fa56ad86631869eef6c8c3850d62b8c17
Curso <Curso@Formacion-TOS13.ipartekaula.com>
2018-09-17 13:57:29 +0200

    limpieza de codigo 2.0

--- youtube/src/main/java/com/ipartek/formacion/youtube/controller/HomeController.java
+++ youtube/src/main/java/com/ipartek/formacion/youtube/controller/HomeController.java
@@ -9,10 +9,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.ipartek.formacion.youtube.model.VideoArrayListDAO;
-import com.ipartek.formacion.youtube.pojo.Usuario;
 import com.ipartek.formacion.youtube.pojo.Video;
 
 /**
--- youtube/src/main/java/com/ipartek/formacion/youtube/controller/LoginController.java
+++ youtube/src/main/java/com/ipartek/formacion/youtube/controller/LoginController.java
@@ -3,7 +3,6 @@ package com.ipartek.formacion.youtube.controller;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
