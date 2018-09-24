da468644271113d267131c1bab19154df708037d
Curso <Curso@Formacion-TOSH3.ipartekaula.com>
2018-09-24 09:59:26 +0200

    HomeControler al arrancar la app

---
+++ supermercado/src/main/java/com/ipartek/formacion/supermercado/controller/HomeController.java
@@ -0,0 +1,62 @@
+package com.ipartek.formacion.supermercado.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeController
+ */
+@WebServlet(
+		urlPatterns = { "/home","/inicio" }, 
+		initParams = { 
+				@WebInitParam(name = "numeroProductos", value = "10", description = "Numero de productos a mostrar en la pagina inicial")
+		})
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request,response);
+	}
+	
+	private void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			request.getRequestDispatcher("home.jsp").forward(request, response);
+		}
+		
+	}
+
+}
