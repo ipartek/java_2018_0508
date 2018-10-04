129fc4516bbdb9de94587191ece5b0d280cf0977
anderuraga <ander.uraga@gmail.com>
2018-10-04 11:01:38 +0200

    cambio controller

--- youtube/src/main/java/com/ipartek/formacion/youtube/controller/back/BackofficeUsuarioController.java
+++ youtube/src/main/java/com/ipartek/formacion/youtube/controller/back/BackofficeUsuarioController.java
@@ -24,7 +24,21 @@ public class BackofficeUsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static UsuarioDAO daoUsuario = null;
 	
-	public static final String OP_ELIMINAR = "34";
+	public static final String OP_LISTAR = "1";
+	public static final String OP_GUARDAR = "2";  //insert id == -1 o update id > 0
+	public static final String OP_ELIMINAR = "3";
+	public static final String OP_IR_FORMULARIO = "4";
+	
+	private static final String VIEW_LISTADO = "usuarios/index.jsp";
+	private static final String VIEW_FORMULARIO = "usuarios/form.jsp";
+	private String view;
+	private Alert alert;
+	
+	private String op; //operacion a realizar
+	private String id;
+	private String nombre;
+	private String password;
+	private String rol;
 	
 	
 	@Override
@@ -111,5 +125,69 @@ public class BackofficeUsuarioController extends HttpServlet {
 		
 	}
 	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try {
+			
+			alert = new Alert();
+			
+			getParameters(request);
+			
+			switch (op) {
+			case OP_ELIMINAR:
+				eliminar(request);
+				break;
+			case OP_IR_FORMULARIO:
+				irFormulario(request);
+				break;	
+			case OP_GUARDAR:
+				guardar(request);
+				break;	
+
+			default:  //LISTAR
+				listar(request);
+				break;
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			view = VIEW_LISTADO;
+			alert = new Alert();
+		}finally {
+			request.setAttribute("alert", alert);
+			request.getRequestDispatcher(view).forward(request, response);
+			
+		}
+		
+	}
+
+	private void listar(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void guardar(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void irFormulario(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void eliminar(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void getParameters(HttpServletRequest request) {
+		
+		op = request.getParameter("op");
+		id = request.getParameter("id");
+		nombre = request.getParameter("nombre");
+		password = request.getParameter("password");
+		rol = request.getParameter("rol");
+	}
 
 }
