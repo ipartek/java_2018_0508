97b33d903060a200fc8e2f8fa21040c824580e5c
Adriana Prado <adriana.prado@opendeusto.es>
2018-10-11 09:17:45 +0200

    Nueva tabla comentario

--- Youtube_2018_0508/Youtube.sql
+++ Youtube_2018_0508/Youtube.sql
@@ -18,6 +18,38 @@ USE `youtube`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `comentario`
+--
+
+DROP TABLE IF EXISTS `comentario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `comentario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` datetime DEFAULT CURRENT_TIMESTAMP,
+  `texto` text NOT NULL,
+  `aprobado` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0 = Sin aprobar\n1 = Aprobado',
+  `id_video` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comentario_has_usuario_idx` (`id_usuario`),
+  KEY `comentario_has_video_idx` (`id_video`),
+  CONSTRAINT `comentario_has_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `comentario_has_video` FOREIGN KEY (`id_video`) REFERENCES `video` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comentario`
+--
+
+LOCK TABLES `comentario` WRITE;
+/*!40000 ALTER TABLE `comentario` DISABLE KEYS */;
+INSERT INTO `comentario` VALUES (2,'2018-10-11 09:10:34','Gran video, me he reido mucho',0,1,2),(3,'2018-10-11 09:12:16','Vaya mierda de video. Me desuscribo',0,1,3);
+/*!40000 ALTER TABLE `comentario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `rol`
 --
 
@@ -58,7 +90,7 @@ CREATE TABLE `usuario` (
   UNIQUE KEY `nombre_UNIQUE` (`nombre`),
   KEY `usuario_has_rol_idx` (`id_rol`),
   CONSTRAINT `usuario_has_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +99,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (4,'user','user',2),(5,'manoli','manoli',2),(6,'pepe','pepe',2),(7,'jorge','jorge',2),(13,'admin','admin',1);
+INSERT INTO `usuario` VALUES (1,'admin','admin',1),(2,'manolo','12345',2),(3,'pepi','12345',2),(4,'user','user',2),(5,'manoli','manoli',2),(6,'pepe','pepe',3),(7,'jorge','jorge',2);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,7 +128,7 @@ CREATE TABLE `video` (
 
 LOCK TABLES `video` WRITE;
 /*!40000 ALTER TABLE `video` DISABLE KEYS */;
-INSERT INTO `video` VALUES (1,'i_cVJgIz_Cs','No te olvides de poner el Where en el Delete From. (Una canción para programadores)',6),(31,'jJllPxkHGPE','Blancanieves y los siete enanitos: Ay ho/Cavar, cavar',6),(32,'UtF6Jej8yb4','Avicii I\'s - The Nights',6),(38,'aL6xNZ9pnbI','Miku Hatsune (Vocaloid) - Senbonzakura',6),(40,'sVwrQ2n13eQ','Autos de choque del FARY',6),(41,'bQMjEkkbHmU','Tijuana In Blue -- Enamorado de la muerte (cover)',6),(42,'Pzqr3rVZNso','EL RENO RENARDO - Camino Moria (videolyric by Azzurro)',6),(43,'p32b5nNq1zw','Iron Maiden - Fear of The Dark (HQ)',6),(44,'NFSyl3pwa-A','\"Maricarmen\" - La Pegatina (videoclip oficial)',6);
+INSERT INTO `video` VALUES (1,'yGnfFW-vyzU','The Devil and The Huntsman - King Arthur Soundtrack',1),(31,'jJllPxkHGPE','Blancanieves y los siete enanitos: Ay ho/Cavar, cavar',6),(32,'UtF6Jej8yb4','Avicii I\'s - The Nights',6),(38,'aL6xNZ9pnbI','Miku Hatsune (Vocaloid) - Senbonzakura',1),(40,'sVwrQ2n13eQ','Autos de choque del FARY',5),(42,'Pzqr3rVZNso','EL RENO RENARDO - Camino Moria (videolyric by Azzurro)',6),(43,'p32b5nNq1zw','Iron Maiden - Fear of The Dark (HQ)',5),(44,'NFSyl3pwa-A','Maricarmen - La Pegatina (videoclip oficial)',6);
 /*!40000 ALTER TABLE `video` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -109,4 +141,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-09 10:45:50
+-- Dump completed on 2018-10-11  9:15:46
