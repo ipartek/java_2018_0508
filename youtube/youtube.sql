e24a7cf1ae78b3484d77890b5f0f6ae30bf9f964
anderuraga <ander.uraga@gmail.com>
2018-10-11 09:21:03 +0200

    nueva tabla "comentario"

--- youtube/youtube.sql
+++ youtube/youtube.sql
@@ -18,6 +18,38 @@ USE `youtube`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `comentario`
+--
+
+DROP TABLE IF EXISTS `comentario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comentario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` datetime DEFAULT CURRENT_TIMESTAMP,
+  `texto` text CHARACTER SET latin1 NOT NULL,
+  `aprobado` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0=Sin aprobar\n1=Aprobado',
+  `id_video` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comentario_has_usuario_idx` (`id_usuario`),
+  KEY `comentario_has_video_idx` (`id_video`),
+  CONSTRAINT `comentario_has_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `comentario_has_video` FOREIGN KEY (`id_video`) REFERENCES `video` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comentario`
+--
+
+LOCK TABLES `comentario` WRITE;
+/*!40000 ALTER TABLE `comentario` DISABLE KEYS */;
+INSERT INTO `comentario` VALUES (1,'2018-10-11 09:13:16','ME he reido mucho, gran video',0,1,2),(2,'2018-10-11 09:14:33','vaya mierda',0,1,3);
+/*!40000 ALTER TABLE `comentario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `rol`
 --
 
@@ -29,7 +61,7 @@ CREATE TABLE `rol` (
   `nombre` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nombre_UNIQUE` (`nombre`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +70,7 @@ CREATE TABLE `rol` (
 
 LOCK TABLES `rol` WRITE;
 /*!40000 ALTER TABLE `rol` DISABLE KEYS */;
-INSERT INTO `rol` VALUES (1,'administrador'),(2,'usuario');
+INSERT INTO `rol` VALUES (1,'administrador'),(7,'otro'),(8,'txerif'),(2,'usuario');
 /*!40000 ALTER TABLE `rol` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -67,7 +99,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'admin','admin',1),(17,'manolo','12345',2),(25,'  maNolito  ','12345',2),(26,'       manolito    ','12345',2),(29,'maria','12345678',2),(34,'Javier','098765432',2),(44,'David','1234',2),(51,'Jose','1234567890',2),(52,'marta','1234567890',2),(53,'joserra','1234567890',2),(54,'DON PIMPOM','12345',2),(57,'TXEMA','12345+6',2),(59,'','',2),(67,'palotes222','',2);
+INSERT INTO `usuario` VALUES (1,'admin','admin',1),(2,'manolo','12345',2),(3,'pepi','12345',2),(29,'maria','12345678',2),(34,'Javier','098765432',2),(44,'David','1234',2),(51,'Jose','1234567890',2),(52,'marta','1234567890',2),(53,'joserra','1234567890',2),(54,'DON PIMPOM','12345',2),(57,'TXEMA','12345+6',2),(59,'','',2),(67,'palotes222','',2);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -87,7 +119,7 @@ CREATE TABLE `video` (
   UNIQUE KEY `codigo_UNIQUE` (`codigo`),
   KEY `video_has_usuario_idx` (`id_usuario`),
   CONSTRAINT `video_has_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,7 +128,7 @@ CREATE TABLE `video` (
 
 LOCK TABLES `video` WRITE;
 /*!40000 ALTER TABLE `video` DISABLE KEYS */;
-INSERT INTO `video` VALUES (1,'i_cVJgIz_Cs','No te olvides de poner el Where en el Delete From. (Una canción para programadores)',1),(3,'Ctwju5_Qflw','Windy Reggae',1),(31,'jJllPxkHGPE','Blancanieves y los siete enanitos: Ay ho/Cavar, cavar',1),(32,'UtF6Jej8yb4','Avicii I\'s - The Nights',1),(33,'4yld459xLJ4','Ramoninos - Ya no quiero ser yo (La Polla Records) (Kalikenyo Rock XII 2017)',1),(38,'aL6xNZ9pnbI','Miku Hatsune (Vocaloid) - Senbonzakura',1),(40,'sVwrQ2n13eQ','Autos de choque del FARY',1),(41,'bQMjEkkbHmU','Tijuana In Blue -- Enamorado de la muerte (cover)',1),(42,'Pzqr3rVZNso','EL RENO RENARDO - Camino Moria (videolyric by Azzurro)',1),(43,'p32b5nNq1zw','Iron Maiden - Fear of The Dark (HQ)',1),(44,'NFSyl3pwa-A','\"Maricarmen\" - La Pegatina (videoclip oficial)',1),(45,'0fgiBri5ZoA','Banda Bassotti - Juri Gagarin',1),(46,'wkjUEumfjFM','Banda Bassotti - Mockba \'993',1),(47,'j--2L3_FN08','SOZIEDAD ALKOHOLIKA - Piedra Contra Tijera (Video Oficial)',1);
+INSERT INTO `video` VALUES (1,'i_cVJgIz_Cs','No te olvides de poner el Where en el Delete From. Maria',29),(31,'jJllPxkHGPE','Blancanieves y los siete enanitos: Ay ho/Cavar, cavar',1),(32,'UtF6Jej8yb4','Avicii I\'s - The Nights',1),(38,'aL6xNZ9pnbI','Miku Hatsune (Vocaloid) - Senbonzakura',1),(40,'sVwrQ2n13eQ','Autos de choque del FARY',1),(41,'bQMjEkkbHmU','Tijuana In Blue -- Enamorado de la muerte (cover)',1),(42,'Pzqr3rVZNso','EL RENO RENARDO - Camino Moria (videolyric by Azzurro)',1),(43,'p32b5nNq1zw','Iron Maiden - Fear of The Dark (HQ)',1),(44,'NFSyl3pwa-A','\"Maricarmen\" - La Pegatina (videoclip oficial)',1),(45,'0fgiBri5ZoA','Banda Bassotti - Juri Gagarin',1),(46,'wkjUEumfjFM','Banda Bassotti - Mockba \'993',1),(47,'j--2L3_FN08','SOZIEDAD ALKOHOLIKA - Piedra Contra Tijera (Video Oficial)',1),(48,'De-W9V-kCos','Marea - La Luna me sabe a poco',44);
 /*!40000 ALTER TABLE `video` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -109,4 +141,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-09 10:48:52
+-- Dump completed on 2018-10-11  9:18:46
