a7c108c604c3f3f43d8df10673a60bd098f8c16f
Adriana Prado <adriana.prado@opendeusto.es>
2018-10-16 11:49:47 +0200

    Usuarios normales redireccionados al inicio, y admins al backoffice

--- Youtube_2018_0508/src/main/java/com/ipartek/formacion/filter/FilterBackoffice.java
+++ Youtube_2018_0508/src/main/java/com/ipartek/formacion/filter/FilterBackoffice.java
@@ -26,9 +26,6 @@ import com.ipartek.formacion.pojo.Usuario;
 @WebFilter(dispatcherTypes = {DispatcherType.REQUEST }
 					, urlPatterns = { "/backoffice/*" })
 public class FilterBackoffice implements Filter {
-
-//	private static final String VIEW_LOGIN = "login.jsp";
-//	private static final String VIEW_HOME = "home";
 	
 	/**
 	 * @see Filter#destroy()
