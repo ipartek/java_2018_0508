b8bc4030ec58bfbe36e62d374f3d04757b830535
Curso <Curso@Formacion-TOS13.ipartekaula.com>
2018-10-18 13:59:00 +0200

    Controlador del home

---
+++ hogwarts/src/main/java/hogwarts/escuela/magia/controller/HomeController.java
@@ -0,0 +1,35 @@
+package hogwarts.escuela.magia.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ComentarioController
+ */
+@WebServlet("/grancomedor")
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+}
