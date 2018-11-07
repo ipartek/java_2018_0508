2595a82cd1c1da6ba836bc36960b4b5295c79963
Adriana Prado <adriana.prado@opendeusto.es>
2018-11-07 13:01:02 +0100

    Gestion de validaciones

--- prestamos_libros/prestamos_libros_api/src/main/java/com/ipartek/formacion/prestamos/api/controller/EditorialesController.java
+++ prestamos_libros/prestamos_libros_api/src/main/java/com/ipartek/formacion/prestamos/api/controller/EditorialesController.java
@@ -4,8 +4,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ipartek.formacion.prestamos_libros.pojo.Editorial;
 import com.ipartek.formacion.prestamos_libros.service.ServiceEditorial;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 
 import java.util.ArrayList;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,15 +26,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class EditorialesController {
 	
 	ServiceEditorial serviceEditorial = null;
+	ValidatorFactory factory = null;
+	Validator validator = null;
 	
 	public EditorialesController() {
 		super();
 		serviceEditorial = ServiceEditorial.getInstance();
+		//Crear Factoria y Validador
+		factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
 	}
 
 	@RequestMapping( method = RequestMethod.GET)
 	public ResponseEntity<ArrayList<Editorial>> listado() {
-		ResponseEntity<ArrayList<Editorial>> response = new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+		ResponseEntity<ArrayList<Editorial>> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		ArrayList<Editorial> editoriales = new ArrayList<Editorial>();
 		
 		try {
@@ -43,7 +55,7 @@ public class EditorialesController {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Editorial> detalle(@PathVariable long id) {
 		
-		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		
 		try {
 			Editorial editorial = serviceEditorial.buscarPorId(id);
@@ -60,18 +72,36 @@ public class EditorialesController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Editorial> crear(@RequestBody Editorial editorial) {
+	public ResponseEntity<Object> crear(@RequestBody Editorial editorial) {
 		
-		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+		ResponseEntity<Object> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		
 		try {
-			if(serviceEditorial.crear(editorial)) {
-				response = new ResponseEntity<>(editorial, HttpStatus.CREATED);
-			}else {
-				response = new ResponseEntity<>(HttpStatus.CONFLICT);
+			
+			Set<ConstraintViolation<Editorial>> violations = validator.validate(editorial);
+			if (violations.isEmpty()) {
+			    /* Ha pasado la validacion*/
+				if(serviceEditorial.crear(editorial)) {
+					response = new ResponseEntity<>(editorial, HttpStatus.CREATED);
+				}else {
+					response = new ResponseEntity<>(HttpStatus.CONFLICT);
+				}
+			}else{
+			    /* Hay fallos, la Validacion no es correcta */
+				ResponseMensaje msj = new ResponseMensaje();
+				msj.setMensaje("Los datos no son válidos.");
+				 for (ConstraintViolation<Editorial> violation : violations) {
+					 msj.addError(violation.getPropertyPath()+": "+violation.getMessage());
+				 }
+				response = new ResponseEntity<>(msj, HttpStatus.CONFLICT);
 			}
-		}catch(Exception e) {
-			//TODO gestionar duplicate key entry
+			
+			
+		}catch(MySQLIntegrityConstraintViolationException e) {
+			ResponseMensaje msj = new ResponseMensaje("Ya existe la editorial, por favor prueba con otro nombre.");
+			response = new ResponseEntity<>(msj, HttpStatus.CONFLICT);
+		}
+		catch(Exception e) {
 			e.printStackTrace();
 		}
 		 
@@ -79,19 +109,35 @@ public class EditorialesController {
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Editorial> modificar(@PathVariable long id, @RequestBody Editorial editorial) {
+	public ResponseEntity<Object> modificar(@PathVariable long id, @RequestBody Editorial editorial) {
 		
-		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+		ResponseEntity<Object> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		
 		try {
+			Set<ConstraintViolation<Editorial>> violations = validator.validate(editorial);
+			
 			editorial.setId(id);
-			if(serviceEditorial.modificar(editorial)) {
-				response = new ResponseEntity<>(editorial, HttpStatus.OK);
-			}else {
-				response = new ResponseEntity<>(HttpStatus.CONFLICT);
+			
+			if (violations.isEmpty()) {
+			    /* Ha pasado la validacion*/
+				if(serviceEditorial.modificar(editorial)) {
+					response = new ResponseEntity<>(editorial, HttpStatus.CREATED);
+				}else {
+					response = new ResponseEntity<>(HttpStatus.CONFLICT);
+				}
+			}else{
+			    /* Hay fallos, la Validacion no es correcta */
+				ResponseMensaje msj = new ResponseMensaje();
+				msj.setMensaje("Los datos no son válidos.");
+				 for (ConstraintViolation<Editorial> violation : violations) {
+					 msj.getErrores().add(violation.getPropertyPath()+": "+violation.getMessage());
+				 }
+				response = new ResponseEntity<>(msj, HttpStatus.CONFLICT);
 			}
+		}catch(MySQLIntegrityConstraintViolationException e) {
+			ResponseMensaje msj = new ResponseMensaje("Ya existe la editorial, por favor prueba con otro nombre.");
+			response = new ResponseEntity<>(msj, HttpStatus.CONFLICT);
 		}catch(Exception e) {
-			//TODO gestionar duplicate key entry
 			e.printStackTrace();
 		}
 		 
@@ -99,16 +145,21 @@ public class EditorialesController {
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Editorial> eliminar(@PathVariable long id) {
+	public ResponseEntity<Object> eliminar(@PathVariable long id) {
 		
-		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+		ResponseEntity<Object> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		
 		try {
 			if(serviceEditorial.eliminar(id)) {
 				response = new ResponseEntity<>(HttpStatus.OK);
 			}else {
-				response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//				response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+				ResponseMensaje msj = new ResponseMensaje("No se puede borrar un registro que no existe");
+				response = new ResponseEntity<>(msj, HttpStatus.NOT_FOUND);
 			}
+		}catch(MySQLIntegrityConstraintViolationException e) {
+			ResponseMensaje msj = new ResponseMensaje("No se puede borrar la editorial porque tiene libros asociados.");
+			response = new ResponseEntity<>(msj, HttpStatus.CONFLICT);
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
