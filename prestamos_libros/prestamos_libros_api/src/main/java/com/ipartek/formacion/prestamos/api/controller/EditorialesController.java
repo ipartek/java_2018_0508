a500ec763bb8fe0750a31467c4682d2cfaee6afc
Curso <Curso@Formacion-TOS12.ipartekaula.com>
2018-11-07 13:08:38 +0100

    Pruebas de errores

--- prestamos_libros/prestamos_libros_api/src/main/java/com/ipartek/formacion/prestamos/api/controller/EditorialesController.java
+++ prestamos_libros/prestamos_libros_api/src/main/java/com/ipartek/formacion/prestamos/api/controller/EditorialesController.java
@@ -1,13 +1,13 @@
 package com.ipartek.formacion.prestamos.api.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.ipartek.formacion.pojo.Alert;
-import com.ipartek.formacion.pojo.Editorial;
-import com.ipartek.formacion.service.ServiceEditorial;
-
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.ArrayList;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,27 +16,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ipartek.formacion.pojo.Editorial;
+import com.ipartek.formacion.service.ServiceEditorial;
 
 @RestController
 @RequestMapping("/editoriales")
 public class EditorialesController {
-	
+
 	ServiceEditorial serviceEditorial = null;
+	ValidatorFactory factory = null;
+	Validator validator = null;
 
 	public EditorialesController() {
 		super();
 		serviceEditorial = ServiceEditorial.getInstance();
+		// Crear Factoria y Validador
+		factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
 	public @ResponseBody ResponseEntity<ArrayList<Editorial>> listar() {
-		
+
 		ArrayList<Editorial> list = new ArrayList<Editorial>();
 		ResponseEntity<ArrayList<Editorial>> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		
+
 		try {
 			list = (ArrayList<Editorial>) serviceEditorial.listar();
-			response = new ResponseEntity<>( list, HttpStatus.OK);
+			response = new ResponseEntity<>(list, HttpStatus.OK);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -50,7 +59,7 @@ public class EditorialesController {
 		try {
 			Editorial editorial = serviceEditorial.buscar(id);
 
-			if (editorial != null && editorial.getId()>0) {
+			if (editorial != null && editorial.getId() > 0) {
 				response = new ResponseEntity<>(editorial, HttpStatus.OK);
 			} else {
 				response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -64,38 +73,54 @@ public class EditorialesController {
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Editorial> eliminar(@PathVariable long id) {
+	public ResponseEntity<Object> eliminar(@PathVariable long id) {
 
-		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		ResponseEntity<Object> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		try {
-			if (serviceEditorial.eliminar(id)) {
+						
+			if ( serviceEditorial.eliminar(id) ) {
 				response = new ResponseEntity<>(HttpStatus.OK);
-			} else {
+			}else {
 				response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
 			}
-		} catch (Exception e) {
+		
+		}catch( SQLIntegrityConstraintViolationException e ) {	
+			response = new ResponseEntity<>(new ResponseMensaje("No se puede eliminar si tiene Libors asociados"), HttpStatus.CONFLICT);
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
-
 		return response;
-
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Editorial> crear(@RequestBody Editorial editorial) {
-		
-		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	public ResponseEntity<Object> crear(@RequestBody Editorial editorial) {
+
+		ResponseEntity<Object> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		try {
-			
-			if (serviceEditorial.crear(editorial)) {
-				response = new ResponseEntity<>(editorial, HttpStatus.CREATED);
-			} else {
-				response = new ResponseEntity<>(HttpStatus.CONFLICT);
+
+			Set<ConstraintViolation<Editorial>> violations = validator.validate(editorial);
+			if(violations.isEmpty()) {
+				 
+				if (serviceEditorial.crear(editorial)) {
+					response = new ResponseEntity<>(editorial, HttpStatus.CREATED);
+				} else {
+					response = new ResponseEntity<>(HttpStatus.CONFLICT);
+				}
+			}else {
+				ResponseMensaje mensaje = new ResponseMensaje("Los datos no son correctos");
+				for (ConstraintViolation<Editorial> violation : violations) {
+					mensaje.addError( violation.getPropertyPath() + ": " + violation.getMessage() );
+					
+					
+				}
+				response = new ResponseEntity<>( mensaje ,  HttpStatus.CONFLICT);
 			}
+
+			
 		} catch (SQLIntegrityConstraintViolationException e) {
-			e.printStackTrace();
+			ResponseMensaje msj = new ResponseMensaje("Ya existe la Editorial, por favor prueba con otro nombre");			
+			response = new ResponseEntity<>(msj, HttpStatus.CONFLICT);
 		} catch (Exception e) {
-			//TODO gestionar excepciones
 			e.printStackTrace();
 		}
 
@@ -104,21 +129,35 @@ public class EditorialesController {
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Editorial> modificar(@PathVariable long id, @RequestBody Editorial editorial) {
+	public ResponseEntity<Object> modificar(@PathVariable long id, @RequestBody Editorial editorial) {
 
-		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		ResponseEntity<Object> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		try {
-			editorial.setId(id);
-
-			if (serviceEditorial.modificar(editorial)) {
-				response = new ResponseEntity<>(editorial,HttpStatus.OK);
-			} else {
-				response = new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
-			}
-		} catch (Exception e) {
+			
+			Set<ConstraintViolation<Editorial>> violations =  validator.validate(editorial);
+			if ( violations.isEmpty() ) {
+			
+				editorial.setId(id);			
+				if ( serviceEditorial.modificar(editorial) ) {
+					response = new ResponseEntity<>(editorial, HttpStatus.OK);
+				}else {
+					response = new ResponseEntity<>(HttpStatus.CONFLICT);
+				}
+				
+			}else {
+				ResponseMensaje mensaje = new ResponseMensaje("Los datos no son correctos");
+				for ( ConstraintViolation<Editorial> v : violations ) {
+					mensaje.addError( v.getPropertyPath() + ": " + v.getMessage() );
+				};
+				response = new ResponseEntity<>( mensaje ,  HttpStatus.CONFLICT);
+			}	
+		
+		}catch (SQLIntegrityConstraintViolationException e) {
+			
+			response = new ResponseEntity<>( new ResponseMensaje("Ya existe la Editorial, por favor prueba con otro nombre")  ,HttpStatus.CONFLICT);
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
 		return response;
 	}
-
 }
---
+++ prestamos_libros/prestamos_libros_api/src/main/java/com/ipartek/formacion/prestamos/api/controller/ResponseMensaje.java
@@ -0,0 +1,46 @@
+package com.ipartek.formacion.prestamos.api.controller;
+
+import java.util.ArrayList;
+
+public class ResponseMensaje {
+	private String mensaje;
+	ArrayList < String > errores = new ArrayList < String > ();
+	
+
+	public ResponseMensaje() {
+		super();
+		this.mensaje="Soy una tetera";
+		this.errores = new ArrayList<String>();
+	}
+
+	public ResponseMensaje(String mensaje) {
+		this();
+		this.mensaje = mensaje;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	public ArrayList<String> getErrores() {
+		return errores;
+	}
+
+	public void setErrores(ArrayList<String> errores) {
+		this.errores = errores;
+	}
+	
+	public void addError(String error) {
+		this.errores.add(error);
+	}
+
+	@Override
+	public String toString() {
+		return "ResponseMensaje [mensaje=" + mensaje + ", errores=" + errores + "]";
+	}	
+	
+}
