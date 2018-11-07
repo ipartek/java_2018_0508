6d8e7a6773f42431ee4d82273e3a2e125680b7a2
guillemoSanchez <[guisanza@gmail.com]>
2018-11-07 09:35:58 +0100

    Modificado Editorial DAO y terminado el EditorialController

--- prestamos_libros_api/prestamos_libros_api/src/main/java/com/ipartek/formacion/prestamos/api/controller/EditorialesController.java
+++ prestamos_libros_api/prestamos_libros_api/src/main/java/com/ipartek/formacion/prestamos/api/controller/EditorialesController.java
@@ -11,89 +11,109 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ipartek.formacion.prestamos_libros.model.EditorialDAO;
 import com.ipartek.formacion.prestamos_libros.pojo.Editorial;
 import com.ipartek.formacion.prestamos_libros.service.ServiceEditorial;
 
 @RestController
 @RequestMapping("/editoriales")
-
 public class EditorialesController {
-	private ServiceEditorial serviceEditorial = ServiceEditorial.getInstance();
 
-	
+	ServiceEditorial serviceEditorial = null;
+
+	public EditorialesController() {
+		super();
+		serviceEditorial = ServiceEditorial.getInstance();
+	}
 
 	@RequestMapping(method = RequestMethod.GET)
-	public @ResponseBody ArrayList<Editorial> listado() {
+	public ResponseEntity<ArrayList<Editorial>> listado() {
 
 		ArrayList<Editorial> list = new ArrayList<Editorial>();
-		
-
+		ResponseEntity<ArrayList<Editorial>> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		try {
-			ServiceEditorial serviceEditorial = new ServiceEditorial();
-		list = (ArrayList<Editorial>) serviceEditorial.listar();
+
+			list = (ArrayList<Editorial>) serviceEditorial.listar();
+			response = new ResponseEntity<>(list, HttpStatus.OK);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return list;
+
+		return response;
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Editorial> detalle(@PathVariable long id) {
 
-	public ResponseEntity<Editorial> detalle(@PathVariable long id) throws Exception {
-		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
-		
-		
-
-		if (id > 0) {
+		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		try {
 			
-			ServiceEditorial serviceEditorial = new ServiceEditorial();
-			Editorial editorial = serviceEditorial.buscarId(id);  
-			response = new ResponseEntity<>(HttpStatus.OK);
+			Editorial editorial = serviceEditorial.buscarId(id);
+			if ( editorial != null && editorial.getId() > 0 ) {
+				response = new ResponseEntity<>(editorial, HttpStatus.OK);
+			}else {
+				response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
 			
-		} else {
-			response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}catch (Exception e) {
+			e.printStackTrace();
 		}
-
 		return response;
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Editorial> eliminar(@PathVariable long id) throws Exception {
-		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+	public ResponseEntity<Editorial> eliminar(@PathVariable long id) {
 
-	serviceEditorial.eliminar(id);
-		
-			response = new ResponseEntity<>(HttpStatus.OK);
-		
-			response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		
-
+		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		try {
+						
+			if ( serviceEditorial.eliminar(id) ) {
+				response = new ResponseEntity<>(HttpStatus.OK);
+			}else {
+				response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 		return response;
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Editorial> crear(@RequestBody Editorial editorial) {
-		ResponseEntity<Editorial> response = null;
 
-		editorial.setId(69);
-
-		response = new ResponseEntity<>(editorial, HttpStatus.OK);
-
+		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		try {
+							
+			if ( serviceEditorial.crear(editorial) ) {
+				response = new ResponseEntity<>(editorial, HttpStatus.CREATED);
+			}else {
+				response = new ResponseEntity<>(HttpStatus.CONFLICT);
+			}
+			
+		}catch (Exception e) {
+			//TODO gestionar duplicate key entry
+			e.printStackTrace();
+		}
 		return response;
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Editorial> modificar(@PathVariable long id, @RequestBody Editorial editorial) {
-		ResponseEntity<Editorial> response = null;
 
-		if (id > 0) {
-
-			response = new ResponseEntity<>(editorial, HttpStatus.OK);
-		} else {
-			response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		ResponseEntity<Editorial> response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		try {
+			editorial.setId(id);				
+			if ( serviceEditorial.modificar(editorial) ) {
+				response = new ResponseEntity<>(editorial, HttpStatus.OK);
+			}else {
+				response = new ResponseEntity<>(HttpStatus.CONFLICT);
+			}
+			
+		}catch (Exception e) {
+			//TODO gestionar duplicate key entry
+			e.printStackTrace();
 		}
-
 		return response;
 	}
 
--- prestamos_libros_model/src/main/java/com/ipartek/formacion/prestamos_libros/model/EditorialDAO.java
+++ prestamos_libros_model/src/main/java/com/ipartek/formacion/prestamos_libros/model/EditorialDAO.java
@@ -3,14 +3,14 @@ package com.ipartek.formacion.prestamos_libros.model;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.ipartek.formacion.prestamos_libros.pojo.Editorial;
 
-
 public class EditorialDAO implements CrudAble<Editorial> {
-	
+
 	private static EditorialDAO INSTANCE = null;
 
 	public EditorialDAO() {
@@ -26,43 +26,48 @@ public class EditorialDAO implements CrudAble<Editorial> {
 
 	@Override
 	public boolean insert(Editorial pojo) throws Exception {
-		String sql = "{ CALL `editorialesInsert` (?) }";
+		String sql = "{ CALL `editorialesInsert` (?,?) }";
 		boolean result = false;
-		try (Connection con = ConnectionManager.getConnection();
-				CallableStatement cs =  con.prepareCall(sql);
-				){
+		try (Connection con = ConnectionManager.getConnection(); CallableStatement cs = con.prepareCall(sql);) {
+
 			cs.setString(1, pojo.getNombre());
-			result = cs.execute();
+			cs.registerOutParameter(2, Types.INTEGER);
+			
+			int affectedRows = cs.executeUpdate(); 
+			if( affectedRows ==  1 ) {
+				
+				pojo.setId(cs.getInt(2));
+				return true;
+			}
+			
 		}
 		return result;
 	}
 
 	@Override
 	public List<Editorial> getAll() throws Exception {
-		
+
 		ArrayList<Editorial> editoriales = new ArrayList<Editorial>();
 		String sql = "{ CALL `editorialesGetAll` () }";
-		try (Connection con = ConnectionManager.getConnection();
-				CallableStatement cs =  con.prepareCall(sql);
-				){
-			//preparar parametros
-			try ( ResultSet rs = cs.executeQuery()){
-				
+		try (Connection con = ConnectionManager.getConnection(); CallableStatement cs = con.prepareCall(sql);) {
+			// preparar parametros
+			try (ResultSet rs = cs.executeQuery()) {
+
 				while (rs.next()) {
 					Editorial e = new Editorial();
 					e.setId(rs.getLong("id"));
 					e.setNombre(rs.getString("nombre"));
 					editoriales.add(e);
-					
+
 				}
-				
+
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
-			
+
 		}
-		
+
 		return editoriales;
 	}
 
@@ -71,9 +76,7 @@ public class EditorialDAO implements CrudAble<Editorial> {
 		Editorial e = new Editorial();
 		String sql = "{ CALL `editorialesGetById`(?) }";
 
-		try (Connection con = ConnectionManager.getConnection();
-				CallableStatement cs =  con.prepareCall(sql);
-				){
+		try (Connection con = ConnectionManager.getConnection(); CallableStatement cs = con.prepareCall(sql);) {
 			cs.setInt(1, Integer.parseInt(id));
 			try (ResultSet rs = cs.executeQuery()) {
 
@@ -89,18 +92,19 @@ public class EditorialDAO implements CrudAble<Editorial> {
 		return e;
 	}
 
-
 	@Override
 	public boolean update(Editorial pojo) throws Exception {
 		String sql = "{ CALL `editorialesUpdate` (?,?) }";
 		boolean result = false;
-		try (Connection con = ConnectionManager.getConnection();
-				CallableStatement cs =  con.prepareCall(sql);
-				){
-			//parseInt para convertir de Long a INT
+		try (Connection con = ConnectionManager.getConnection(); CallableStatement cs = con.prepareCall(sql);) {
+			// parseInt para convertir de Long a INT
 			cs.setInt(1, new Long(pojo.getId()).intValue());
 			cs.setString(2, pojo.getNombre());
-			result = cs.execute();
+			
+			int affectedRows = cs.executeUpdate();
+			if ( affectedRows == 1 ) {
+				result = true;
+			}
 		}
 		return result;
 	}
@@ -109,16 +113,13 @@ public class EditorialDAO implements CrudAble<Editorial> {
 	public boolean delete(String id) throws Exception {
 		String sql = "{ CALL `editorialesDelete` (?) }";
 		boolean result = false;
-		try (Connection con = ConnectionManager.getConnection();
-				CallableStatement cs =  con.prepareCall(sql);
-				){
-			//parseInt para convertir de String a INT
+		try (Connection con = ConnectionManager.getConnection(); CallableStatement cs = con.prepareCall(sql);) {
+			// parseInt para convertir de String a INT
 			cs.setInt(1, Integer.parseInt(id));
-			result = cs.execute();
+			if ( cs.executeUpdate() == 1 ) {
+				result = true;
+			}
 		}
 		return result;
 	}
-
-	
-
 }
