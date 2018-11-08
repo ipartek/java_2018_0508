c5460b78306d3f131e2931fcbc538ec5aca8eb80
guillemoSanchez <[guisanza@gmail.com]>
2018-11-08 10:54:33 +0100

    subir Javascript

--- Prestamos/prestamos_libros_client/js/main.js
+++ Prestamos/prestamos_libros_client/js/main.js
@@ -1,42 +1,71 @@
+const ENDPOINT = "http://localhost:8080/PrestamosAPI/";
+var ulEditoriales = document.getElementById("ulEditoriales");
+var mensaje = document.getElementById("mensaje");
+var editoriales = [];
+var editorial;
+
 window.addEventListener("load", function(event) {
+  console.log("Todos los recursos terminaron de cargar, comenzamos a jugar!"); 
+  //varciar lista
+  ulEditoriales.innerHTML = "";
 
-    console.log("Todos los recursos terminaron de cargar, comenzamos a jugar!");
+  //llamada Ajax para obtener Editoriales
+  var request = new XMLHttpRequest();
 
-    const ENDPOINT = "http://localhost:8080/PrestamosAPI/";
-    var ulEditoriales = document.getElementById('ulEditoriales');
-    var mensaje = document.getElementById('mensaje');
-    var editoriales = [];
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        console.log("response 200 " + request.responseText);
+        editoriales = JSON.parse(request.responseText);
+        console.log("editoriales %o", editoriales);
 
-    //varciar lista
-    ulEditoriales.innerHTML = "";
+        var lis = "";
+        editoriales.forEach((editorial, index) => {
+          lis += `<li onclick="editorialClick(${index}, event)" class="list-group-item"> ${
 
-    //llamada Ajax para obtener Editoriales
-    var request = new XMLHttpRequest();
+            editorial.id
+          } ${editorial.nombre} </li>`;
+        });
+        ulEditoriales.innerHTML = lis;
+        mensaje.textContent = "";
+      }
+    }
+  };
 
-    request.onreadystatechange = function() {
-
-        if( request.readyState === 4 ){
-
-            if ( request.status === 200 ){
-
-                console.log('response 200 '  + request.responseText);
-                editoriales = JSON.parse(request.responseText);
-                console.log('editoriales %o', editoriales );
-
-                var lis = "";
-                editoriales.forEach( editorial => {                    
-                    lis += `<li> ${editorial.id} ${editorial.nombre} </li>`;
-                });
-                ulEditoriales.innerHTML = lis;
-                mensaje.textContent = '';
-            }
-
-        }
-        
-    };    
-
-    request.open('GET', ENDPOINT + 'editoriales');
-    request.send();
-
-
+  request.open("GET", ENDPOINT + "editoriales");
+  request.send();
 });
+function editorialClick(posicion, event) {
+  console.log(`editorialClick ${posicion}`);
+  //event.target.parentElement.children.forEach().remove('active');
+  document
+    .querySelectorAll("#ulEditoriales li")
+    .forEach(el => el.classList.remove("active"));
+  event.target.classList.add("active");
+}
+
+
+function crear() {
+  var nombre = document.getElementById("iNombre").value;
+  console.log(`click crear nombre= ${nombre}`);
+
+var data = {"nombre": nombre}; //jason enviar
+
+  var request = new XMLHttpRequest(); //llamada Ajax
+
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      if (request.status === 201) {
+        console.log("response 201 " + request.responseText);
+        editoriales = JSON.parse(request.responseText);
+      }
+      if (request.status === 409) {
+        console.warn("409 Conflicto" + request.responseText);
+      }
+    }
+  };
+
+  request.open('POST', ENDPOINT + 'editoriales'); 
+  request.setRequestHeader('content-type','application/json')
+  request.send(JSON.stringify(data));//formato JSON 
+}
