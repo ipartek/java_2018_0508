4a278272643740d9ea3e74ef5d2ace299b871e13
anderuraga <ander.uraga@gmail.com>
2018-11-14 12:10:00 +0100

    incluido swagger para documentar API

--- prestamos_libros/prestamos_libros_api/src/main/java/com/ipartek/formacion/prestamos/api/controller/PrestamosController.java
+++ prestamos_libros/prestamos_libros_api/src/main/java/com/ipartek/formacion/prestamos/api/controller/PrestamosController.java
@@ -1,30 +1,20 @@
 package com.ipartek.formacion.prestamos.api.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.ipartek.formacion.prestamos_libros.pojo.Editorial;
-import com.ipartek.formacion.prestamos_libros.pojo.Prestamo;
-import com.ipartek.formacion.prestamos_libros.service.ServiceEditorial;
-import com.ipartek.formacion.prestamos_libros.service.ServicePrestamo;
-
-import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.ArrayList;
-import java.util.Set;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
 
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ipartek.formacion.prestamos_libros.pojo.Prestamo;
+import com.ipartek.formacion.prestamos_libros.service.ServicePrestamo;
+
+import io.swagger.annotations.ApiOperation;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -43,6 +33,7 @@ public class PrestamosController {
 	
 	}
 
+	@ApiOperation(value = "Listado Prestamos")
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<ArrayList<Prestamo>> listado( @RequestParam(name="activos", required=false, defaultValue="-1" ) int activos) {
 
