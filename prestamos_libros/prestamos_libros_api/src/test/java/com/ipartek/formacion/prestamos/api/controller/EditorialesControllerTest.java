37568f7634368b999198880b705a946f1da56971
anderuraga <ander.uraga@gmail.com>
2018-11-15 10:57:25 +0100

    junit funcionando con jndi

--- prestamos_libros/prestamos_libros_api/src/test/java/com/ipartek/formacion/prestamos/api/controller/EditorialesControllerTest.java
+++ prestamos_libros/prestamos_libros_api/src/test/java/com/ipartek/formacion/prestamos/api/controller/EditorialesControllerTest.java
@@ -1,6 +1,9 @@
 package com.ipartek.formacion.prestamos.api.controller;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.File;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -11,14 +14,28 @@ import org.springframework.http.ResponseEntity;
 
 import com.ipartek.formacion.prestamos_libros.pojo.Editorial;
 
+import hthurow.tomcatjndi.TomcatJNDI;
+
 public class EditorialesControllerTest {
 	
+	static final String PATH_CONTEXT_FILE = "C:\\desarrollo\\workspace\\java_2018_0508\\prestamos_libros\\prestamos_libros_api\\src\\main\\webapp\\META-INF\\context.xml";
 	static private EditorialesController controller;
 	static private Editorial eMock;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		
 		controller = new EditorialesController();
+		
+		//cargar contex.xml para el DataSource del ConnectionManager
+		TomcatJNDI tomcatJNDI = new TomcatJNDI();
+		File contextFile = new File(PATH_CONTEXT_FILE);
+		if ( contextFile.exists() ) {
+			tomcatJNDI.processContextXml(contextFile);
+			tomcatJNDI.start();		
+		}else{
+			fail("No existe fichero configuracion bbdd path:" + PATH_CONTEXT_FILE );
+		}	
 	}
 
 	@AfterClass
@@ -41,7 +58,7 @@ public class EditorialesControllerTest {
 		
 		ResponseEntity<Object> response = controller.crear(eMock);
 		
-		assertEquals( 201 , response.getStatusCode() );
+		assertEquals( 201 , response.getStatusCode().value() );
 		
 		
 	}
