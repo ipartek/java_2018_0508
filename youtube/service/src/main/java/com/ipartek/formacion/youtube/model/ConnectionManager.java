362bf40a9f0ecc81d6ff0cba8a93dba093c9f399
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2018-11-20 13:36:25 +0100

    bla

--- youtube/service/src/main/java/com/ipartek/formacion/youtube/model/ConnectionManager.java
+++ youtube/service/src/main/java/com/ipartek/formacion/youtube/model/ConnectionManager.java
@@ -2,9 +2,6 @@ package com.ipartek.formacion.youtube.model;
 
 import java.sql.Connection;
 
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-
 public class ConnectionManager {
 
 	private static Connection conn;
@@ -13,20 +10,9 @@ public class ConnectionManager {
 
 		conn = null;
 
-		try {
-			InitialContext ctx = new InitialContext();
-			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/mydb");
+		
 
-			if (ds == null) {
-				throw new Exception("Data source no encontrado!");
-			}
-
-			conn = ds.getConnection();
-
-		} catch (Exception e) {
-
-			e.printStackTrace();
-		}
+		
 
 		return conn;
 
