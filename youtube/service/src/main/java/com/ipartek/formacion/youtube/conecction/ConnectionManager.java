132b47c769200636f540b2da29a2c80a1bee1fd5
Curso <Curso@Formacion-TOS11.ipartekaula.com>
2018-11-21 09:03:52 +0100

    Añadido log4j para las trazas

--- youtube/service/src/main/java/com/ipartek/formacion/youtube/conecction/ConnectionManager.java
+++ youtube/service/src/main/java/com/ipartek/formacion/youtube/conecction/ConnectionManager.java
@@ -5,26 +5,36 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Properties;
 
-public class ConnectionManager {
+import org.apache.log4j.Logger;
 
+public class ConnectionManager {
+	private final static Logger LOG = Logger.getLogger(ConnectionManager.class);
 	private static Connection conn;
 
-	public static Connection getConnection() throws Exception {
+	public static Connection getConnection() {
 
-		conn = null;
+		try {
+			conn = null;
 
-		// cargar properties
-		Properties prop = new Properties();
+			// cargar properties
+			Properties prop = new Properties();
 
-		InputStream input = ConnectionManager.class.getClassLoader().getResourceAsStream("bbdd.properties");
-		prop.load(input);
+			InputStream input = ConnectionManager.class.getClassLoader().getResourceAsStream("bbdd.properties");
+			prop.load(input);
+			LOG.debug("Cargado fichero properties");
 
-		// comprobar que exista .jar para mysql
-		Class.forName(prop.getProperty("bbdd.driver")).newInstance();
+			// comprobar que exista .jar para mysql
+			Class.forName(prop.getProperty("bbdd.driver")).newInstance();
+			LOG.debug("Existe driver mySQL");
 
-		// crear conexion
-		conn = DriverManager.getConnection(prop.getProperty("bbdd.url"), prop.getProperty("bbdd.user"),
-				prop.getProperty("bbdd.password"));
+			// crear conexion
+			conn = DriverManager.getConnection(prop.getProperty("bbdd.url"), prop.getProperty("bbdd.user"),
+					prop.getProperty("bbdd.password"));
+			LOG.debug("Conexión establecida con la base de datos");
+
+		} catch (Exception e) {
+			LOG.fatal("Error estableciendo la conexión con base de datos", e);
+		}
 
 		return conn;
 
