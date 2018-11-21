efb223971ca198b6bd76f2837346f347df514a85
anderuraga <ander.uraga@gmail.com>
2018-11-21 12:26:18 +0100

    proyecto rest montado y funcionando UsuarioController y Swagger

---
+++ youtube/api-rest/src/main/java/com/ipartek/formacion/youtube/api/controller/ResponseMensaje.java
@@ -0,0 +1,52 @@
+package com.ipartek.formacion.youtube.api.controller;
+
+import java.util.ArrayList;
+
+public class ResponseMensaje {
+	
+	private String mensaje;
+	private ArrayList<String> errores;
+
+	public ResponseMensaje() {
+		super();
+		this.mensaje = "Soy una tetera";
+		this.errores = new ArrayList<>();
+	}
+
+	public ResponseMensaje(String mensaje) {
+		this();
+		this.mensaje = mensaje;
+	}
+
+	public ResponseMensaje(String mensaje, ArrayList<String> errores) {
+		this();
+		this.mensaje = mensaje;
+		this.errores = errores;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	public ArrayList<String> getErrores() {
+		return errores;
+	}
+
+	public void setErrores(ArrayList<String> errores) {
+		this.errores = errores;
+	}
+	
+	public void addError(String error) {
+		this.errores.add(error);
+	}
+
+	@Override
+	public String toString() {
+		return "ResponseMensaje [mensaje=" + mensaje + ", errores=" + errores + "]";
+	}
+		
+}
